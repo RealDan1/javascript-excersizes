120355e1ffb7879ef0c1921d38bb2ab087269ec2
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-03-26 10:23:58 +0200

    Experiment backtick string type

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// bigint
+let a = 123456789012345678901234567890123456789123451340n;
+console.log(a);
+console.log(typeof a);
 
-
-let b = 123456789012345678901234567890123456789123451340n;
-console.log(b);
-console.log(typeof b);
+// backtick strings(extended capability)
+let name1 = "John Smith";
+console.log(`Your name is ${name1}`);
