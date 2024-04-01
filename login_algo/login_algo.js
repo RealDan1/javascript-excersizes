282d10f9fab21bece0135ff0d2c346732551f51b
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-04-01 14:56:43 +0200

    Finish first login algorithm

--- login_algo/login_algo.js
+++ login_algo/login_algo.js
@@ -1,17 +1,17 @@
-const userName = prompt("Please enter username");
+let userName = prompt("Please enter username");
 
-if (userName = "Admin") {
-    const passwInput = prompt("Enter Password");
-    if (passwInput = "Master") {
+if (userName === "Admin") {
+    let passwInput = prompt("Enter Password");
+    if (passwInput === "Master") {
         alert("Welcome Big boss");
-    } else if ((passwInput = "null") || (passwInput="")) {
+    } else if ((passwInput === "null") || (passwInput==="")) {
         alert("Cancelled");
     } else if (passwInput) {
         alert("Wrong Password");
     }
     
-} else if (userName = null || "") {
-    alert("cancelled");
+} else if (userName === null || userName ==="") {
+    alert("Cancelled");
 } else if (userName){
     alert("I don't know you");
 } else {
