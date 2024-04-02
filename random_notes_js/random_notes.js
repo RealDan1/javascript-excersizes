29d7ddba7fd87d5d14f81d88118651593629e69b
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-04-02 13:32:16 +0200

    ternary

--- random_notes_js/random_notes.js
+++ random_notes_js/random_notes.js
@@ -96,3 +96,13 @@ console.log (!null);
 //     alert("-1");
 // }
 
+//Ternary
+let result;
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+
+let result = (a + b < 4)? below : over ;
