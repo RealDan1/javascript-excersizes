efaeac22ed6b3643b09b0855f04a0386eb07e2ab
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-04-14 12:31:54 +0200

    Add modify elements files - to test one part of excersize not working (Excersize maybe bugged? WIP)

---
+++ modifying_elements/modify_element.js
@@ -0,0 +1,7 @@
+// write your code below
+const button = document.querySelector("#my-button");
+const link = document.querySelector("#hide-link");
+console.log(button);
+console.log(link);
+button.innerText = "Click me!";
+link.classList.add(".d-none");
