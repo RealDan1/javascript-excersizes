5dd7f2665b498cf5d5bbed353ae6e2be3aa96a36
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-04-14 16:59:53 +0200

    Fix Modify Elements code
    NB no .dot in link.classList.add("d-none");

--- modifying_elements/modify_element.js
+++ modifying_elements/modify_element.js
@@ -1,11 +1,6 @@
-// write your code below
 const button = document.querySelector("#my-button");
-const link = document.querySelector("#hide-link");
-console.log(button);
-console.log(link);
-button.innerText = "Click me!";
-link.classList.add(".d-none");
-
-button.addEventListener("click", () {
-    button.innerText("Clicked!")
-})
+    const link = document.querySelector("#hide-link");
+    console.log(button);
+    console.log(link);
+    link.classList.add("d-none");
+    button.innerText = "Click me!";
