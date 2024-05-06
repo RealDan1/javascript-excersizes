f7d960133287515b3452246dffa9fdc55edde7fb
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-05-06 13:02:26 +0200

    Add .slice to replace end comma with full stop

--- guestlist_filter/guestlist_filter.js
+++ guestlist_filter/guestlist_filter.js
@@ -12,17 +12,24 @@ const people = [
 
 const admitted = document.querySelector(".admitted");
 const refused = document.querySelector(".refused");
-admitted.textContent = "Admit: ";
-refused.textContent = "Refuse: ";
+// admitted.textContent = "Admit: ";
+// refused.textContent = "Refuse: ";
+let toAdmit = "Admit: ";
+let toRefuse = "Refuse: ";
 
 // loop starts here
 for (const person of people) {
     if (person === "Phil" || person === "Lola") {
-        refused.textContent += person + ", ";
+        toRefuse += person + ", ";
     } else {
-        admitted.textContent += person + ", ";
+        toAdmit += person + ", ";
     }
 }
 
+toAdmit = toAdmit.slice(0, -2) + ".";
+toRefuse = toRefuse.slice(0, -2) + ".";
+
+refused.textContent = toRefuse;
+admitted.textContent = toAdmit;
 // refused.textContent += ;
 // admitted.textContent += ;
