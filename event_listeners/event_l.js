847c0089423fe2b2e70c3899153b16f8e771f7b4
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-05-20 15:11:24 +0200

    Advanced loop through all buttons and add an event listener
    (which will in turn run a function for each button)

--- event_listeners/event_l.js
+++ event_listeners/event_l.js
@@ -1,11 +1,11 @@
-const btn1 = document.querySelector('#btn1');
-const btn2 = document.querySelector('#btn2');
+// const btn1 = document.querySelector('#btn1');
+// const btn2 = document.querySelector('#btn2');
 
-btn2.onclick = () => console.log('you shortcutted clicking');
+// btn2.onclick = () => console.log('you shortcutted clicking');
 
-function alertFunction() {
-  console.log('you clicked a button!');
-}
+// function alertFunction() {
+//   console.log('you clicked a button!');
+// }
 
 // btn1.addEventListener('click', alertFunction);
 
@@ -13,6 +13,16 @@ function alertFunction() {
 //   console.log(e.target);
 // });
 
-btn1.addEventListener('click', function (e) {
-  e.target.style.background = 'blue';
+// btn1.addEventListener('click', function (e) {
+//   e.target.style.background = 'blue';
+// });
+
+//Advanced loop through all buttons and add an event listener
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((buttonX) => {
+  buttonX.addEventListener('click', () => {
+    console.log(buttonX.id);
+  });
 });
