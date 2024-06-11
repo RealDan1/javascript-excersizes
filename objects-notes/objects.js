61c3d689540ac455f9ae049533e969c00dbc01be
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-06-11 16:13:36 +0200

    Object Numbers and naming note

--- objects-notes/objects.js
+++ objects-notes/objects.js
@@ -50,3 +50,11 @@ function makeUser(name, age, height) {
     height,
   };
 }
+
+let obj23 = {
+  0: 'test', // same as "0": "test"
+};
+
+// both alerts access the same property (the number 0 is converted to string "0")
+console.log(obj23['0']); // test
+console.log(obj23[0]); // test (same property)
