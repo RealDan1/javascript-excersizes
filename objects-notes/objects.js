aec8c47497b05300d4b1243a6389955fae7a75d0
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-06-12 13:40:55 +0200

    Add excersize - test to see if object is ENTIRELY empty

--- objects-notes/objects.js
+++ objects-notes/objects.js
@@ -99,37 +99,55 @@ for (let code in codes) {
   console.log(+code); // 49, 41, 44, 1
 }
 
-// Summary
-// ========
-// Objects are associative arrays with several special features.
-// They store properties (key-value pairs), where:
+// SUMMARY
+// =========
+// 1. Objects are associative arrays with several special features.
 // - Property keys must be strings or symbols (usually strings).
 // - Values can be of any type.
 
-// To access a property, we can use:
+// 2. To access a property, we can use:
 // - The dot notation: obj.property.
 // - Square brackets notation obj["property"]. Square brackets allow taking the key from a variable, like obj[varWithKey].
 
-// Additional operators:
+// 3. Additional operators:
 // - To delete a property: delete obj.prop.
 // - To check if a property with the given key exists: "key" in obj.
 // - To iterate over an object: for (let key in obj) loop.
 
-// What we’ve studied in this chapter is called a “plain object”, or just Object.
+// 4. What we’ve studied in this chapter is called a “plain object”, or just Object.
 // There are many other kinds of objects in JavaScript:
 // - Array to store ordered data collections,
 // - Date to store the information about the date and time,
 // - Error to store the information about an error.
 // …And so on.
 
-// They have their special features that we’ll study later. Sometimes people say something like “Array type” or “Date type”, but formally they are not types of their own, but belong to a single “object” data type. And they extend it in various ways.
+// 5. They have their special features that we’ll study later. Sometimes people say something like “Array type” or “Date type”, but formally they are not types of their own, but belong to a single “object” data type. And they extend it in various ways.
 
-// Objects in JavaScript are very powerful. Here we’ve just scratched the surface of a topic that is really huge. We’ll be closely working with objects and learning more about them in further parts of the tutorial.
+// 6. Objects in JavaScript are very powerful. Here we’ve just scratched the surface of a topic that is really huge. We’ll be closely working with objects and learning more about them in further parts of the tutorial.
 
 // Objects Excersizes:
 // ==================
+// 1.
 let user = {};
-user = {
-  name: 'John',
-  surname: 'smith',
-};
+
+user.name = 'John';
+user.surname = 'Smith';
+user.name = 'Pete';
+
+delete user.name;
+
+//2. test if object has ANY property whatsoever:
+function isEmpty(obj) {
+  for (let key in obj) {
+    // if the loop has started, there is a property
+    return false;
+  }
+  return true;
+}
+let schedule = {};
+
+console.log(isEmpty(schedule)); // true
+
+schedule['8:30'] = 'get up';
+
+console.log(isEmpty(schedule)); // false
