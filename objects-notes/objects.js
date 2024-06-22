b8fafbd8589c72f3b8b1a1cef095e30eba1e4248
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-06-22 10:16:35 +0200

    Add Reduce array method

--- objects-notes/objects.js
+++ objects-notes/objects.js
@@ -273,3 +273,11 @@ const arr2 = [1, 2, 3, 4, 5];
 const oddNums = arr.filter(isOdd);
 console.log(oddNums); // Outputs [1, 3, 5];
 console.log(arr); // Outputs [1, 2, 3, 4, 5], original array is not affected
+
+//reduce method(not fully understood)
+arr = [1, 2, 3, 4, 5];
+const productOfAllNums = arr.reduce((total, currentItem) => {
+  return total * currentItem;
+}, 1);
+console.log(productOfAllNums); // Outputs 120;
+console.log(arr); // Outputs [1, 2, 3, 4, 5]
