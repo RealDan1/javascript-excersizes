b51003dc78d299ee06702248fe0ec9a94ae89333
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-08 13:21:53 +0200

    Add prettier

--- random-notes/random_notes.js
+++ random-notes/random_notes.js
@@ -4,7 +4,7 @@ console.log(a);
 console.log(typeof a);
 
 // Backtick strings(extended capability)
-let name1 = "John Smith";
+let name1 = 'John Smith';
 console.log(`Your name is ${name1}`);
 
 // Escape character '\'
@@ -12,17 +12,17 @@ const bigmouth = "I've got no right to take my place…";
 console.log(bigmouth);
 
 // String method - charAt(position)
-let text = "HELLO WORLD";
+let text = 'HELLO WORLD';
 let char = text.charAt(0);
 console.log(char);
 
 // String method - at()
-const name2 = "W3Schools";
+const name2 = 'W3Schools';
 let letter1 = name2.at(2);
 console.log(letter1);
 
 // String method - Property Access [ ] - same technique as at
-const name3 = "W3Schools";
+const name3 = 'W3Schools';
 console.log(name3[3]);
 
 // There are 3 methods for extracting a part of a string:
@@ -30,17 +30,17 @@ console.log(name3[3]);
 // slice(start, end) - note the end char is cut off(not included)
 // substring(start, end)
 // substr(start, length)
-text = "Apple, Banana, Kiwi";
+text = 'Apple, Banana, Kiwi';
 let part = text.slice(7, 13);
 console.log(part);
 
 // Negative counts from end of string
-text = "Apple, Banana, Kiwi";
+text = 'Apple, Banana, Kiwi';
 part = text.slice(-12, -6);
 console.log(part);
 
 // TrimStart() and trimEnd();
-text = "      Apple, Banana";
+text = '      Apple, Banana';
 let trimmedText = text.trimStart();
 console.log(trimmedText);
 let pad = trimmedText.padStart(20, 0); // count is total char count, not just adding a number of times
@@ -48,35 +48,35 @@ console.log(pad);
 text = text.trimStart();
 
 //replace()
-let replaced = text.replace("Banana", "Strawberry");
+let replaced = text.replace('Banana', 'Strawberry');
 console.log(replaced);
 
 // Replace all matches in string (\g global)
 // and case insensitive (\i insensitive)
 // gi combines both
-text = "Banana Banana banana banana";
-replaced = text.replace(/Banana/gi, "Strawberry");
+text = 'Banana Banana banana banana';
+replaced = text.replace(/Banana/gi, 'Strawberry');
 console.log(replaced);
 // replaced = text.replace(/Banana/i, "Strawberry");
 // console.log(replaced);
 // No need for this code anymore^
 
 // first array:
-text = "Hello world";
-let myArray = text.split("");
+text = 'Hello world';
+let myArray = text.split('');
 console.log(typeof myArray);
 
 // first if statement:
 let time = new Date().getHours();
-let greeting = "Good evening";
+let greeting = 'Good evening';
 if (time < 18 && time > 11) {
-    greeting = "Good Day";
-    console.log(greeting);
+  greeting = 'Good Day';
+  console.log(greeting);
 } else if (time < 11 && time > 0) {
-    greeting = "Good morning";
-    console.log(greeting);
+  greeting = 'Good morning';
+  console.log(greeting);
 } else {
-    console.log(greeting);
+  console.log(greeting);
 }
 
 // Test not
@@ -106,60 +106,60 @@ console.log(!null);
 // Small ternary test
 a = 1;
 let b = 2;
-let result = a + b < 4 ? "below" : "over";
+let result = a + b < 4 ? 'below' : 'over';
 console.log(result);
 
 // Test switch/case:
 let x = 20;
 switch (x) {
-    case 3:
-        console.log("its one");
-        break;
-    case 20:
-    case 22:
-        console.log("its 20 or 22");
-        break;
+  case 3:
+    console.log('its one');
+    break;
+  case 20:
+  case 22:
+    console.log('its 20 or 22');
+    break;
 }
 
 // First function: format test
 function nameAndAge(name, age) {
-    return "hello my name is " + name + ", and my age is " + age + ".";
+  return 'hello my name is ' + name + ', and my age is ' + age + '.';
 }
-console.log(nameAndAge("alex", 24));
+console.log(nameAndAge('alex', 24));
 // Try something quick - make a console log function:
 function logThis1(text1) {
-    return console.log(text1);
+  return console.log(text1);
 }
 
-logThis1(nameAndAge("daneel", 24));
+logThis1(nameAndAge('daneel', 24));
 // Wow it worked...
 
 //function to perform action
 function testFunction(name) {
-    message = "hello, " + name;
-    console.log(message);
+  message = 'hello, ' + name;
+  console.log(message);
 }
-testFunction("hamsworth");
+testFunction('hamsworth');
 
 // Function to return a value
 function returnMe() {
-    let value = "its a lonely world";
-    return value;
+  let value = 'its a lonely world';
+  return value;
 }
 console.log(returnMe());
 
 // Test arrow func tion:
 const namevar = (name1) => `Hello, ${name1}!`;
-const greet = namevar("Dan1");
+const greet = namevar('Dan1');
 console.log(greet);
 
 // Fucntion w empty return that stops the function halfway
 function age_test(age) {
-    if (age <= 18) {
-        console.log("are you sure you are old enough?");
-        return;
-    }
-    console.log("Proceed");
+  if (age <= 18) {
+    console.log('are you sure you are old enough?');
+    return;
+  }
+  console.log('Proceed');
 }
 age_test(18);
 
@@ -178,7 +178,7 @@ age_test(18);
 
 // 1
 function add7(n) {
-    return n + 7;
+  return n + 7;
 }
 console.log(add7(3));
 
@@ -188,17 +188,17 @@ console.log(multiply(2, 4));
 
 // 3
 function capitalize(word) {
-    word = word.toLowerCase();
-    return word.charAt(0).toUpperCase() + word.slice(1);
+  word = word.toLowerCase();
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
-console.log(capitalize("signAl"));
+console.log(capitalize('signAl'));
 
 // 4
 function lastLetter(word) {
-    let stringCount = word.length;
-    return word.charAt(stringCount - 1);
+  let stringCount = word.length;
+  return word.charAt(stringCount - 1);
 }
-console.log(lastLetter("abcd"));
+console.log(lastLetter('abcd'));
 
 // Testing map() in arrays:
 
@@ -206,7 +206,7 @@ console.log(lastLetter("abcd"));
 //     return string.toUpperCase();
 //   }
 
-const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+const cats = ['Leopard', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
 
 const upperCats = cats.map((string) => string.toUpperCase());
 // I refined the code here by =>
@@ -215,9 +215,9 @@ console.log(upperCats);
 
 // Test for loop even outputter
 for (let i = 2; i <= 10; i++) {
-    if (i % 2 == 0) {
-        //alert(i);
-    }
+  if (i % 2 == 0) {
+    //alert(i);
+  }
 }
 
 // Replace for with while
@@ -261,8 +261,8 @@ for (let i = 2; i <= 10; i++) {
 // console.log(printString);
 
 //countdowntimer_________________________
-let output = document.querySelector(".output");
-output.innerHTML = "";
+let output = document.querySelector('.output');
+output.innerHTML = '';
 
 //set countdown here
 let i = 15;
@@ -272,19 +272,19 @@ let i = 15;
 // output.appendChild(para);
 
 for (let j = i; j >= 0; j--) {
-    if (j == i) {
-        const para = document.createElement("p");
-        para.textContent = "Countdown: " + j;
-        output.appendChild(para);
-    } else if (j == 0) {
-        const para = document.createElement("p");
-        para.textContent = j + " Blastoff!!";
-        output.appendChild(para);
-    } else if (j >= 0 && j < i) {
-        const para = document.createElement("p");
-        para.textContent = j;
-        output.appendChild(para);
-    }
+  if (j == i) {
+    const para = document.createElement('p');
+    para.textContent = 'Countdown: ' + j;
+    output.appendChild(para);
+  } else if (j == 0) {
+    const para = document.createElement('p');
+    para.textContent = j + ' Blastoff!!';
+    output.appendChild(para);
+  } else if (j >= 0 && j < i) {
+    const para = document.createElement('p');
+    para.textContent = j;
+    output.appendChild(para);
+  }
 }
 
-// test push_____________________
+// Template literals, testing new line??
