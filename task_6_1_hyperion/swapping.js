2cac41f94e7850ec78d1706d165d39cafd68fc70
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-10 13:11:03 +0200

    feat: finish swapping string algorithm

--- task_6_1_hyperion/swapping.js
+++ task_6_1_hyperion/swapping.js
@@ -1,5 +1,19 @@
 let number = prompt('Please enter a number of atleast three digits');
 
+let secondDigit = number[1]; // fetches secondDigit
+let lastDigit = number.at(number.length - 1);
+
+let newNumber = '';
 for (let i = 0; i < number.length; i++) {
-  const element = array[i];
+  if (i === 1) {
+    //if replace second number
+    newNumber += number.at(number.length - 1);
+  } else if (i === number.length - 1) {
+    //replace last number
+    newNumber += number.at(1);
+  } else {
+    //else build new string as normal
+    newNumber += number[i];
+  }
 }
+console.log(newNumber);
