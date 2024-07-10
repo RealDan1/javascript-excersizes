d9b56902cd24d932dbcb382cbd3d6975374f3454
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-10 13:11:34 +0200

    refactor: remove comments

--- task_6_1_hyperion/swapping.js
+++ task_6_1_hyperion/swapping.js
@@ -1,18 +1,15 @@
 let number = prompt('Please enter a number of atleast three digits');
 
-let secondDigit = number[1]; // fetches secondDigit
+let secondDigit = number[1];
 let lastDigit = number.at(number.length - 1);
 
 let newNumber = '';
 for (let i = 0; i < number.length; i++) {
   if (i === 1) {
-    //if replace second number
     newNumber += number.at(number.length - 1);
   } else if (i === number.length - 1) {
-    //replace last number
     newNumber += number.at(1);
   } else {
-    //else build new string as normal
     newNumber += number[i];
   }
 }
