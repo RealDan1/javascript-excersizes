fd702d55664a6730d37fdcb450bd22fae34c72c5
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-11 11:07:30 +0200

    refactor: add comments

--- task_6_2/palindrome.js
+++ task_6_2/palindrome.js
@@ -1,12 +1,17 @@
 let inputWord = prompt('Please enter a word');
+//create a new string for the reverse word since strings are immutable
 let reverseWord = '';
 
+//initialise while loop to length of the input word
 let i = inputWord.length;
 while (i > 0) {
+  //iterate backwards from the length of the word to zero
   i--;
+  //add each letter to the new string in this order(i.e. in reverse order)
   reverseWord += inputWord[i];
 }
 
+//check if palindrome or not
 if (inputWord === reverseWord) {
   console.log(`${inputWord} is a palindrome`);
 } else if (inputWord !== reverseWord) {
