f6e1ac83c07df31dc856f678ff334a7f3d887a34
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-11 11:34:15 +0200

    feat: add input code

---
+++ task_7_1/guestList.js
@@ -0,0 +1,7 @@
+let names = [];
+//loop input for all 10 names
+for (let i = 0; i < 9; i++) {
+  names[i] = prompt(
+    'Input a name of a person you would like to invite to the dinner party'
+  );
+}
