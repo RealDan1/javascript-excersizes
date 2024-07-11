e4b5d22a77000869e516ebe2bda4a2b9a880148e
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-11 12:09:53 +0200

    feat: add input verification

--- task_7_1/guestList.js
+++ task_7_1/guestList.js
@@ -1,7 +1,20 @@
 let names = [];
-//loop input for all 10 names
-for (let i = 0; i < 10; i++) {
+//loop input for all 10 names and add to the array
+
+outerloop: for (let i = 0; i < 10; i++) {
   names[i] = prompt(
     'Input a name of a person you would like to invite to the dinner party'
   );
+
+  //check if input contains any numbers(thus not a name in russia and certain american states apparently)
+  for (let j = 0; j < 10; j++) {
+    //j is inner loop, i is outer loop
+    if (names[i].includes(j)) {
+      alert(
+        'No numbers are allowed, please input only letters for the names. Please refresh the page and try again'
+      );
+      //break to outerloop so that it doesnt carry on asking to input more names
+      break outerloop;
+    }
+  }
 }
