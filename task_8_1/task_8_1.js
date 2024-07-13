0123d5d48a16c13456e07a4921eb55f61616efca
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-13 16:11:58 +0200

    feat; add subtractNumbers

--- task_8_1/task_8_1.js
+++ task_8_1/task_8_1.js
@@ -13,11 +13,13 @@ let findSum = (sumArray) => {
 //...rest will not accept an array and function the same way as findSum()
 //but will accept indefinite individual arguments and add them together.
 //So for the purpose of this excersize ignore it, my submission to this question is just the above standard findSum().
-//Apologies I document all my excersizes for later.
-let findSumRest = (...rest) => {
+//Apologies I document all my excersizes on Github for later.
+const findSumRest = (...rest) => {
   let total = 0;
-  for (item of rest) {
+  for (const item of rest) {
     total += item;
   }
   return total;
 };
+
+const subtractNumbers = (a, b) => a - b;
