118ba34abe01170fc89677cd8a928d5f87b5c9ac
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-14 13:41:04 +0200

    finish task 8_2

---
+++ task_8_2/digitalHideSeek.js
@@ -0,0 +1,17 @@
+//define hide with location as argument
+const hide = (location) => {
+  //store location in hideLocation
+  let hideLocation = location;
+  //create new function inside hide called seek
+  const seek = () => {
+    //reference a variable outside the seek function but inside the hide() function, meaning it forms a closure because hide() will have already executed(closed) but seek can still access hide's variables, even though it closed.
+    return hideLocation; // accessing the variable in this case just returns it but we could have done other things with the variable in the function body (processed it another way). Is this all correct?
+  };
+  return seek; //returns seek function(without () means it doesnt call/activate the function, it just reutrns the reference to the function);
+};
+
+let startGame = hide('under the table'); // place hide into startgame with argument: "under the table", effectively placing seek into startGame - now startGame waits a call, which will effectively call seek - seek has closure over the hideLocation variable, which itself long ago stored the location variable - which itself now contains the string of "under the table", when we input it into the function as we called it in this line(Line 13). Is this correct?
+
+console.log(startGame()); //here is the call
+
+console.log(hideLocation); //returns reference Error because hideLocation is defined within the local scope of the hide function and thus is not accessible in the global scope.
