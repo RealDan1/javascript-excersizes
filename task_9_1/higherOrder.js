ffed512cb83f03a0021ca395bdd362cbf6ad57f1
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-17 12:20:12 +0200

    feat: task 9_1 attempt 2 WIP

--- task_9_1/higherOrder.js
+++ task_9_1/higherOrder.js
@@ -11,15 +11,31 @@ inputArray = [
   'Ava',
 ];
 
-let myFilterFunction = (arr) => (fn) => {
+// let myFilterFunction = (arr) => (fn) => {
+//   const arrayAfterFilter = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == 6) {
+//       arrayAfterFilter.push(fn(arr[i]));
+//     }
+//   }
+//   return arrayAfterFilter;
+// };
+
+// startArray = myFilterFunction(inputArray);
+// console.log(startArray());
+
+let myFilterFunction = (arr, fn) => {
   const arrayAfterFilter = [];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == 6) {
-      arrayAfterFilter.push(fn(arr[i]));
-    }
+    arrayAfterFilter.push(fn(arr[i]));
   }
   return arrayAfterFilter;
 };
 
-startArray = myFilterFunction(inputArray);
-console.log(startArray());
+const filterType = (item) => {
+  if (item.length === 6) {
+    return item;
+  }
+};
+
+console.log(myFilterFunction(inputArray));
