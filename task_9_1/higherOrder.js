b8bb5c77d9d7a2c9630b612ed0fe0e8da91d217d
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-22 16:12:40 +0200

    refactor: remake function to ommit all dropped filter elements

--- task_9_1/higherOrder.js
+++ task_9_1/higherOrder.js
@@ -11,33 +11,10 @@ inputArray = [
   'Ava',
 ];
 
-// let myFilterFunction = (arr) => (fn) => {
-//   const arrayAfterFilter = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] == 6) {
-//       arrayAfterFilter.push(fn(arr[i]));
-//     }
-//   }
-//   return arrayAfterFilter;
-// };
-
-// startArray = myFilterFunction(inputArray);
-// console.log(startArray());
-
-//write this in long form so i understand it
-// let myFilterFunction = (arr) => (fn) => {
-//   const arrayAfterFilter = [];
-//   for (let i = 0; i < arr.length; i++) {
-//     arrayAfterFilter.push(fn(arr[i]));
-//   }
-//   return arrayAfterFilter;
-// };
-
 //I wrote this task with the filter function partly in long form so I could understand the logic better.
 
 //create filterFunction
 let myLongFilterFunction = function (arr) {
-  //filter function must return another function (which is anonymous), it uses a function as its argument(fn which contains the function that )
   return (fn) => {
     const arrayAfterFilter = [];
     for (let i = 0; i < arr.length; i++) {
