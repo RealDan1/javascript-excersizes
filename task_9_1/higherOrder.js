0bc68922fa5c4c0dbaa8602f357c6e5983ef2ac5
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-23 11:03:20 +0200

    feat: add working higher order function

--- task_9_1/higherOrder.js
+++ task_9_1/higherOrder.js
@@ -11,23 +11,60 @@ inputArray = [
   'Ava',
 ];
 
-//I wrote this task with the filter function partly in long form so I could understand the logic better.
+//I wrote this task with the filter function partly in long form so I could understand the logic better. The shortform arrow function in the hyperion pdf was a bit confusing.
+
+// //create filterFunction
+// let myFilterFunction = function (arr) {
+//   return (fn) => {
+//     const arrayAfterFilter = [];
+//     //iterate through the whole input array
+//     for (let i = 0; i < arr.length; i++) {
+//       //if the input function is given the input array item and returns true
+//       if (fn(arr[i])) {
+//         //add the current item to the new array
+//         arrayAfterFilter.push(arr[i]);
+//       }
+//     }
+//     //return the new array for output
+//     return arrayAfterFilter;
+//   };
+// };
+
+// //callback function that returns true if input string length is six
+// const lengthIsSix = (item) => {
+//   if (item.length === 6 && typeof item === 'string') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// //output the function plus the required arguments
+// console.log(myFilterFunction(inputArray)(lengthIsSix));
 
 //create filterFunction
-let myLongFilterFunction = function (arr) {
-  return (fn) => {
-    const arrayAfterFilter = [];
-    for (let i = 0; i < arr.length; i++) {
-      arrayAfterFilter.push(fn(arr[i]));
+let myFilterFunction = function (arr, fn) {
+  const arrayAfterFilter = [];
+  //iterate through the whole input array
+  for (let i = 0; i < arr.length; i++) {
+    //if the callback function is given the input array item and returns true
+    if (fn(arr[i])) {
+      //add the current item to the new array
+      arrayAfterFilter.push(arr[i]);
     }
-    return arrayAfterFilter;
-  };
+  }
+  //return the new array for output
+  return arrayAfterFilter;
 };
 
-const filterType = (item) => {
-  if (item.length === 6) {
-    return item;
+//callback function that returns true if input string length is six
+const lengthIsSix = (item) => {
+  if (item.length === 6 && typeof item === 'string') {
+    return true;
+  } else {
+    return false;
   }
 };
 
-console.log(myLongFilterFunction(inputArray)(filterType));
+//output the function plus the required arguments
+console.log(myFilterFunction(inputArray)(lengthIsSix));
