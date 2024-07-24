936de3b8169ce5d8992c3fe036dbfeb87bfcfeab
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-24 10:46:09 +0200

    feat: whoops finished 9_2 in like 5 minutes lol

--- task_9_2/callBack.js
+++ task_9_2/callBack.js
@@ -5,16 +5,22 @@
 let intervalID;
 let counter = 0;
 
-let startCounter = function() {
-    intervalID = setInterval(
-        // Create the logic needed in the setInterval() function for
-        //incrementing the counter and outputting to the console
-    );
-}
+let startCounter = function () {
+  intervalID = setInterval(
+    () => {
+      console.log(counter);
+      counter += 1;
+    },
+    1000
+    // Create the logic needed in the setInterval() function for
+    //incrementing the counter and outputting to the console
+  );
+};
 
 function stopCounter() {
-    // add the logic required to stop the counter
-    // using clearInterval()
+  clearInterval(intervalID);
+  // add the logic required to stop the counter
+  // using clearInterval()
 }
 
 let startButton = document.getElementById('start');
@@ -22,9 +28,8 @@ let stopButton = document.getElementById('stop');
 
 // The event listener below is missing a callback function argument;
 // this button should start the outputting of the counter to the console
-startButton.addEventListener("click", );
+startButton.addEventListener('click', startCounter);
 
 // The event listener below is missing a callback function argument;
 // this button should the counter from outputting to the console
-stopButton.addEventListener("click", );
-
+stopButton.addEventListener('click', stopCounter);
