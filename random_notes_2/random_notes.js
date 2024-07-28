8b68b72cfd17cdad4fbd241079579ccdf9a3fa8e
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-28 11:59:59 +0200

    feat: add car sort by most recent manufactured

--- random_notes_2/random_notes.js
+++ random_notes_2/random_notes.js
@@ -15,3 +15,32 @@ function printName(player) {
 }
 
 printName(playerTwo);
+
+//evauluate cars in objects:
+//==========================
+// Defined a constructor function to create car objects
+function Car(brand, model, year, colour) {
+  this.brand = brand;
+  this.model = model;
+  this.year = year;
+  this.colour = colour;
+}
+// Created 3 car objects using the Car constructor
+let car1 = new Car('Tesla', 'Model S', 2023, 'Gray');
+
+let car2 = new Car('Audi', 'e-tron', 2022, 'Red');
+let car3 = new Car('Porsche', 'Taycan', 2021, 'Blue');
+// Create an array to store the car objects
+let cars = [car1, car2, car3];
+// Define a function to find the most recently manufactured car
+function findNewestCar(array) {
+  // Sort the array of cars based on the manufacturing year in descending order
+  array.sort((firstCar, secondCar) =>
+    firstCar.year > secondCar.year ? -1 : 1
+  );
+  // Use template and string literals to display the details
+  console.log(`The most recently manufactured car is the ${array[0].brand}
+ ${array[0].model}, which was made in ${array[0].year}`);
+}
+// Call the findNewestCar function
+findNewestCar(cars);
