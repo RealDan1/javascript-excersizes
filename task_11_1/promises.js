e1a4cbc3db657758eb7f5ae76a37cbcd0f3ba502
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-01 16:24:29 +0200

    feat: remove test console output

--- task_11_1/promises.js
+++ task_11_1/promises.js
@@ -4,16 +4,19 @@ fetch('https://pokeapi.co/api/v2/pokemon/pikachu/')
   //convert response to a json object
   .then((res) => res.json())
   //output results
-  .then((result) => {
-    pokemon = result;
-    console.log(
-      `name:
+  .then(
+    (result) => {
+      pokemon = result;
+      console.log(
+        `Name:
 ${pokemon.name}
 
-weight:
+Weight:
 ${pokemon.weight}`
-    );
-  });
-
-//delete this:
-console.log('test this text');
+      );
+    },
+    (error) => {
+      // Return an error in the console
+      console.log(error);
+    }
+  );
