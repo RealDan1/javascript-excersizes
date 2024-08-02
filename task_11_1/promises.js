3405b3602f1e25140cec7bc962fc85fe28ed45df
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-02 15:13:07 +0200

    feat: add abilities displayer (WIP)
    now displays the string name correctly but it should list the whole object and all its contents - the ability object

--- task_11_1/promises.js
+++ task_11_1/promises.js
@@ -7,20 +7,36 @@ fetch('https://pokeapi.co/api/v2/pokemon/pikachu/')
   .then((result) => {
     // pass result(the object) into pokemon array
     pokemon = result;
+
     //log the details of the pokemon object neatly
     console.log(
       `Name:\n${pokemon.name}
         \nWeight:\n${pokemon.weight}
-        \n${pokemon.abilities.ability[0].name}
-
+        \nAbilities:
         `
+
+      // \n${pokemon.abilities.ability[0].name}
     );
-    let abilities1 = pokemon.abilities;
-    for (const ability in abilities1) {
-      console.log(`\nAbilities:\n${ability.name}`);
+    let abilities = pokemon.abilities;
+    for (const ability of abilities) {
+      console.log(`\n${ability.ability.name}`);
     }
+    // let abilities1 = pokemon.abilities;
+    // for (const ability in abilities1) {
+    //   console.log(`\nAbilities:\n${ability.name}`);
+    // }
   })
   //catch the error at any point a promise is rejected and log it to the console
   .catch((error) => {
     console.log(error);
   });
+
+// fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const abilities = data.abilities;
+//     for (const ability of abilities) {
+//       console.log(ability.ability.name); // access the ability name
+//       console.log(ability.is_hidden); // access if the ability is hidden
+//     }
+//   });
