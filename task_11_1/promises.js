3b6494aa855c7761f94daaac7b0f56c532b0fd0a
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-03 15:08:22 +0200

    feat: finish task 11_1 main body

--- task_11_1/promises.js
+++ task_11_1/promises.js
@@ -15,10 +15,13 @@ fetch('https://pokeapi.co/api/v2/pokemon/pikachu/')
         \nAbilities:
         `
     );
-    let abilities = pokemon.abilities;
-    for (const ability of abilities) {
-      console.log(`\n${ability.ability.name}`);
-    }
+    // QUESTION LINE: why wouldnt this work on my work computer but it works now on my home computer and in the output dialog box when i hit run code in code runner extension on vscode??
+    let abilities1 = pokemon.abilities;
+    console.log(abilities1);
+    // console.log(JSON.stringify(abilities));
+    // for (const ability of abilities) {
+    //   console.log(`\n${ability.ability.name}`);
+    // }
   })
   //catch the error at any point a promise is rejected and log it to the console
   .catch((error) => {
