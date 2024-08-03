b12e92944be49a254cdcaa3f36424a617408f593
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-03 15:16:06 +0200

    feat: correct object template literal part

--- task_11_1/promises.js
+++ task_11_1/promises.js
@@ -13,9 +13,10 @@ fetch('https://pokeapi.co/api/v2/pokemon/pikachu/')
       `Name:\n${pokemon.name}
         \nWeight:\n${pokemon.weight}
         \nAbilities:
-        ${pokemon.abilities}
         `
     );
+    // Log pokemon.abilities in a seperate console.log call because JS handles complex objects inside template literals strangely - this was not mentioned in the task and perhaps should be mentioned otherwise it prints [object Object] if placed inside a template literal.
+    console.log(pokemon.abilities);
   })
   // catch the error at any point a promise is rejected and log it to the console
   .catch((error) => {
