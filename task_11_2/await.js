322145228c74777c2288168e3509e4f9b540932e
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-09 13:15:56 +0200

    test 2.4

--- task_11_2/await.js
+++ task_11_2/await.js
@@ -4,7 +4,7 @@ async function fetchCats() {
       'http://thecatapi.com/api/images/get?format=src&type=gif'
     );
     // catUrl = catUrl.json();
-
+//
     console.log(catUrl.url);
   } catch (error) {
     console.log(error);
