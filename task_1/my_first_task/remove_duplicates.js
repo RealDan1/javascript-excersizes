d20bd42739fffc5dd198cd63b181d201bfe7a0f6
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-17 17:50:33 +0200

    feat: add lodash.uniq method

--- task_1/my_first_task/remove_duplicates.js
+++ task_1/my_first_task/remove_duplicates.js
@@ -1,1 +1,5 @@
 const lodash = require('lodash');
+const array = [1, 2, 10, 100, 10, 2, 5, 6, 10, 1000, 7, 2, 100, 1, 5, 7, 10];
+
+//use the uniq method to print out the array minus the duplicates
+console.log(lodash.uniq(array));
