25875d07be526f93a103dbeb36f764741cb979da
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-22 19:21:16 +0200

    feat: remove redundancies and add core task 3 file

---
+++ task_3/src/App.js
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <button href="www.linkedin.com">Linkedin</button>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
