d36787598cc9ad6aa55aaa3030fb979bfd40dfa8
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-22 19:59:56 +0200

    feat: add target blank for link

--- task_3/src/App.js
+++ task_3/src/App.js
@@ -3,7 +3,10 @@ import './styles.css';
 export default function App() {
   return (
     <div className="App">
-      <button href="https://www.linkedin.com/in/daneel-van-der-merwe-518062238/">
+      <button
+        href="https://www.linkedin.com/in/daneel-van-der-merwe-518062238/"
+        target="blank"
+      >
         LinkedIn
       </button>
       <h1>22 September 2024 19:31</h1>
