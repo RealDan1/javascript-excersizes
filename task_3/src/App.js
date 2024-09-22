8a4298b7e66487167c235e4f4c767236000e3b6e
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-22 19:57:39 +0200

    feat: add date and time

--- task_3/src/App.js
+++ task_3/src/App.js
@@ -3,8 +3,10 @@ import './styles.css';
 export default function App() {
   return (
     <div className="App">
-      <button href="www.linkedin.com">Linkedin</button>
-      <h2>Start editing to see some magic happen!</h2>
+      <button href="https://www.linkedin.com/in/daneel-van-der-merwe-518062238/">
+        LinkedIn
+      </button>
+      <h1>22 September 2024 19:31</h1>
     </div>
   );
 }
