21d28023fb23289feaa428d9b40493ae6fef1dda
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-25 16:09:37 +0200

    feat: add red border bootstrap boxes

--- react-cra-first/src/App.js
+++ react-cra-first/src/App.js
@@ -4,9 +4,14 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import LayoutExample from './components/LayoutExample';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LayoutExample />
+    </div>
+  );
 }
 
 export default App;
