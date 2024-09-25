10bfb562c953eb797bce299cff8755e2fa5c1753
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-25 14:05:12 +0200

    Feat: Add custom component

--- vite-app-first/src/App.jsx
+++ vite-app-first/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import Heading from './components/Heading';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Heading Test</h1>
+      <Heading />
       <p>Paragraph Test</p>
     </>
   );
