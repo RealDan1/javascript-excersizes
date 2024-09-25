dbd49c8c31af111f1ab28d519f1940b2b94702e4
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-25 15:26:28 +0200

    Add Bootstrap and vite buttons

--- vite-app-first/src/App.jsx
+++ vite-app-first/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Heading from './components/Heading';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,11 @@ function App() {
     <>
       <Heading name="John" age={23} />
       <p>Paragraph Test</p>
+      <button>A Vite standard Button</button>
+      <hr />
+      <Button> A Boostrap Button</Button>
+      <hr />
+      <Button variant="outline-secondary">a Bootstrap Outline Button</Button>
     </>
   );
 }
