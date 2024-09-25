6440167d98c8d60731228981491175af2451a8bf
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-25 15:46:29 +0200

    feat: start new app for react css study

---
+++ react-cra-first/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
--- vite-app-first/src/App.jsx
+++ vite-app-first/src/App.jsx
@@ -3,23 +3,28 @@ import './App.css';
 import Heading from './components/Heading';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div id="app-container">
-      <Heading name="John" age={23} />
-      <p>Paragraph Test</p>
-      <button>A Vite standard Button</button>
-      <hr />
-      <Button> A Bootstrap Button</Button>
-      <hr />
-      <Button variant="outline-secondary">a Bootstrap Outline Button</Button>
-      <hr />
-      <Button variant="success">a Bootstrap Success Button</Button>
-      <hr />
-      <Button variant="link">Link</Button>
+      <Container>
+        <Heading name="John" age={23} />
+        <p>Paragraph Test</p>
+        <button>A Vite standard Button</button>
+        <hr />
+        <Button> A Bootstrap Button</Button>
+        <hr />
+        <Button variant="outline-secondary">a Bootstrap Outline Button</Button>
+        <hr />
+        <Button variant="success">a Bootstrap Success Button</Button>
+        <hr />
+        <Button variant="link">Link</Button>
+      </Container>
     </div>
   );
 }
