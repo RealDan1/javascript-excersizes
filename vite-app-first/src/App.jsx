a5b401f15dd535eb30978931603540998bee6053
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-25 15:33:57 +0200

    feat: add app.css back

--- vite-app-first/src/App.jsx
+++ vite-app-first/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './App.css';
 import Heading from './components/Heading';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
@@ -7,15 +8,19 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div id="app-container">
       <Heading name="John" age={23} />
       <p>Paragraph Test</p>
       <button>A Vite standard Button</button>
       <hr />
-      <Button> A Boostrap Button</Button>
+      <Button> A Bootstrap Button</Button>
       <hr />
       <Button variant="outline-secondary">a Bootstrap Outline Button</Button>
-    </>
+      <hr />
+      <Button variant="success">a Bootstrap Success Button</Button>
+      <hr />
+      <Button variant="link">Link</Button>
+    </div>
   );
 }
 
