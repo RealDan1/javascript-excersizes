7bfb8c61d5af4be566a7a689c0ea4b64947082b9
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-25 16:26:43 +0200

    feat: add stack

--- react-cra-first/src/App.js
+++ react-cra-first/src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import LayoutExample from './components/LayoutExample';
+import StackExample from './components/StackExample';
 
 function App() {
   return (
     <div className="App">
       <LayoutExample />
+      <StackExample />
     </div>
   );
 }
