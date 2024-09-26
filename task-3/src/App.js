ea64fa42f59d3db20532ed4df0904b4689b44ec4
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-09-26 13:00:07 +0200

    feat: add task 3

--- task-3/src/App.js
+++ task-3/src/App.js
@@ -10,6 +10,40 @@ export default function App() {
         <button>LinkedIn</button>
       </a>
       <h1>22 September 2024 19:31</h1>
+      <ul>
+        <li>
+          React (and other frameworks/libraries) make coding less tedious than
+          using vanilla JS because it is declarative. It lets you say what you
+          want the UI to look like and then react handles the how to do it
+          part.writing vanilla JS as a beginner would normally take much more
+          work.
+        </li>
+        <li>
+          You can create buttons, forms, or sections as components and use them
+          again and again without rewriting the same code.
+        </li>
+        <li>
+          React lets you split your UI into small, reusable pieces called
+          components, so you don’t have to write everything in one big file.
+          This makes it easier to maintain and update the code.
+        </li>
+        <li>
+          Frameworks and libraries provide preset and template methods for
+          workflow so you can focus less on the code and more on what you want
+          the end design to look like.
+        </li>
+        <li>
+          React for example has effective ways of managing the state of a
+          website, so you can make pages dynamic more easily than if you where
+          to use vanilla JS.
+        </li>
+
+        <li>
+          Frameworks and Libraries make it easier to handle manage large scale
+          projects. it allows modular collaboration and updating of the code.
+        </li>
+        <li></li>
+      </ul>
     </div>
   );
 }
