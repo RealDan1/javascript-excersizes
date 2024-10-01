c7152b301baa2d6dc1ec1f17f631914e322e19f5
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-01 16:13:41 +0200

    feat: add boilerplate for Hero

--- task-4/src/App.js
+++ task-4/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
+import Hero from './Components/Hero';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
-      <hr />
+      <hr id="hr-nav" />
+      <Hero></Hero>
     </div>
   );
 }
