c42b42f1fc3ceac0ebb6960107f62d003921e178
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-02 15:53:28 +0200

    feat: ready submission

---
+++ task-4/task-4/src/Components/Hero.jsx
@@ -0,0 +1,20 @@
+import './Hero.css';
+
+// use a prop in this function
+export default function Hero(props) {
+  return (
+    <div className="Hero">
+      <div className="Hero-content">
+        {/* using the prop to dynamically change the text of the Hero */}
+        <h1>{props.title}</h1>
+        <p>A place to read, write, and deepen your understanding</p>
+        <button>
+          <a href="#" className="Hero-button">
+            Start reading
+          </a>
+        </button>
+      </div>
+      <div className="Hero-image"></div>
+    </div>
+  );
+}
