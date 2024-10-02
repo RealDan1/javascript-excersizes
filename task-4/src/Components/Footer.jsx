d78bdfa12130c23cd3b7b0eb71703fd7253d4f61
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-02 15:51:42 +0200

    feat: add link

--- task-4/src/Components/Footer.jsx
+++ task-4/src/Components/Footer.jsx
@@ -3,30 +3,35 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer>
-      <li>
-        <a href="">About</a>
-      </li>
-      <li>
-        <a href="">Careers</a>
-      </li>
-      <li>
-        <a href="">Press</a>
-      </li>
-      <li>
-        <a href="">Blog</a>
-      </li>
-      <li>
-        <a href="">Privacy</a>
-      </li>
-      <li>
-        <a href="">Terms</a>
-      </li>
-      <li>
-        <a href="">Text to speech</a>
-      </li>
-      <li>
-        <a href="">Teams</a>
-      </li>
+      <div>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Careers</a>
+        </li>
+        <li>
+          <a href="">Press</a>
+        </li>
+        <li>
+          <a href="">Blog</a>
+        </li>
+        <li>
+          <a href="">Privacy</a>
+        </li>
+        <li>
+          <a href="">Terms</a>
+        </li>
+        <li>
+          <a href="">Text to speech</a>
+        </li>
+        <li>
+          <a href="">Teams</a>
+        </li>
+      </div>
+      <a id="clone-link" href="https://medium.com/" target="_blank">
+        URL of original website: www.medium.com
+      </a>
     </footer>
   );
 }
