e738942ca363c30be0c2a803e39d2c604772b42d
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-02 15:16:39 +0200

    feat: add all components

---
+++ task-4/src/Components/Footer.jsx
@@ -0,0 +1,32 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      <li>
+        <a href="">About</a>
+      </li>
+      <li>
+        <a href="">Careers</a>
+      </li>
+      <li>
+        <a href="">Press</a>
+      </li>
+      <li>
+        <a href="">Blog</a>
+      </li>
+      <li>
+        <a href="">Privacy</a>
+      </li>
+      <li>
+        <a href="">Terms</a>
+      </li>
+      <li>
+        <a href="">Text to speech</a>
+      </li>
+      <li>
+        <a href="">Teams</a>
+      </li>
+    </footer>
+  );
+}
