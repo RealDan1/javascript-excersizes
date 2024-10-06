eca42482a2f4e5ac99c024ee98da2fb59fe60481
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-06 16:45:23 +0200

    feat: attempt fix setCount

--- react-state-intro/src/App.js
+++ react-state-intro/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Count from './components/Count';
+// Create the App component
 function App() {
   return (
-    
+    <div className="App">
+      {/* Import the count component */}
+      <Count />
+    </div>
   );
 }
-
 export default App;
