43d1a162e1ba137f0582d1b2a11215da8ccdeb27
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-06 18:46:32 +0200

    feat: add NameInput

--- react-state-intro/src/App.jsx
+++ react-state-intro/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
 import Count from './components/Count';
+import NameInput from './components/NameInput';
 // Create the App component
 function App() {
   return (
     <div className="App">
       {/* Import the count component */}
       <Count />
+      <hr />
+      <NameInput />
     </div>
   );
 }
