5a13cd61f8a27655aafb26fe7ee0915c67b6e941
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-06 16:45:23 +0200

    feat: attempt fix setCount

---
+++ react-state-intro/src/components/Count.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { useState } from 'react';
+
+export default function Count() {
+  const [count, setCount] = useState(0);
+  // Event handler for the onClick event
+  function increaseCount() {
+    setCount(count + 1);
+    console.log('Event Triggered');
+  }
+  // Create a variable called “count” and set the initial value to 0
+
+  return (
+    <div>
+      {/* use JSX code to display the value of the count variable */}
+      <h1>Count : {count}</h1>
+      {/* Button to increase the value of the count variable */}
+      <button onClick={increaseCount}> Increase Count </button>
+    </div>
+  );
+}
