06e0bdefa2995c70f1da9372c0612a7ce21ae437
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-07 14:35:02 +0200

    feat: add prop destructuring

--- react-state-intro/src/components/NameInput.jsx
+++ react-state-intro/src/components/NameInput.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-export default function NameInput(props) {
+export default function NameInput({ name, handleChange }) {
   // Create a name state object with a default value
   return (
     <div>
       <label for="nameInput">Enter Name: </label>
       <input
-        onChange={(event) => props.handleChange(event.target.value)}
+        onChange={(event) => handleChange(event.target.value)}
         //  Input component using onChange to update the value of the state
         id="nameInput"
         placeholder="Enter name here"
-        defaultValue={props.userName}
+        defaultValue={name}
       />
       {/* Display the value of the userName State variable */}
-      <h3> Name : {props.userName}</h3>
+      <h3> Name : {name}</h3>
     </div>
   );
 }
