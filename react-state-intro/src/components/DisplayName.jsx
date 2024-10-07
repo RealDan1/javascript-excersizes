04b519330cd6eaa631aab21be898a5fdba844d6f
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-07 13:45:32 +0200

    feat: add DisplayName

---
+++ react-state-intro/src/components/DisplayName.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Welcome from './Welcome.jsx';
+import NameInput from './NameInput';
+import { useState } from 'react';
+
+export default function DisplayName() {
+  // Create the name state variable
+  const [userName, setUserName] = useState('');
+  // Create a function to handle the state change
+  function handleChange(userInput) {
+    setUserName(userInput);
+  }
+  return (
+    <div>
+      {/* The userName state is passed to the Welcome component as a prop */}
+      <Welcome name={userName} />
+      {/* The userName state is passed to the NameInput component as a prop
+          The handleChange function is passed to the component as a prop */}
+      <NameInput name={userName} handleChange={handleChange} />
+    </div>
+  );
+}
