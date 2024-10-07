f304a1b3c748469fd3a19b28a039ee4d95a010c2
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-07 16:04:30 +0200

    feat: finish skeleton code for balance update

--- task-5/ReactComponents/react-interest-calculator/src/App.jsx
+++ task-5/ReactComponents/react-interest-calculator/src/App.jsx
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Balance from './components/Balance';
 function App() {
   return (
     <div>
--- task-5/ReactComponents/react-interest-calculator/src/components/Balance.jsx
+++ task-5/ReactComponents/react-interest-calculator/src/components/Balance.jsx
@@ -1,6 +1,31 @@
-const [balance, setBalance] = useState(0);
+import React, { useState } from 'react';
 
 export default function Balance() {
-  return;
-  <h1>Balance: {balance}</h1>;
+  let [balance, setBalance] = useState(0);
+
+  const [inputAmount, setInputAmount] = useState(0);
+  //function to handle the input change
+  function handleInput(e) {
+    setInputAmount(e.target.value);
+  }
+
+  function increaseNumber() {
+    setBalance(balance + Number(inputAmount));
+  }
+  function decreaseNumber() {
+    setBalance(balance - Number(inputAmount));
+  }
+  return (
+    <div>
+      <h1>Balance:{balance} </h1>
+      <input
+        type="number"
+        value={inputAmount}
+        onChange={handleInput}
+        placeholder="Enter a number"
+      />
+      <button onClick={increaseNumber}>Deposit</button>
+      <button onClick={decreaseNumber}>Withdraw</button>
+    </div>
+  );
 }
