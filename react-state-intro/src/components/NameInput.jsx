5fd9a9a826beec49c44b37ce9783bea94665d989
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-07 13:59:18 +0200

    feat: add app.jsx components

--- react-state-intro/src/components/NameInput.jsx
+++ react-state-intro/src/components/NameInput.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { useState } from 'react';
 
-export default function NameInput({ handleChange, userName }) {
+export default function NameInput(props) {
   // Create a name state object with a default value
   return (
     <div>
       <label for="nameInput">Enter Name: </label>
       <input
-        onChange={(event) => handleChange(event.target.value)}
+        onChange={(event) => props.handleChange(event.target.value)}
         //  Input component using onChange to update the value of the state
         id="nameInput"
         placeholder="Enter name here"
-        defaultValue={userName}
+        defaultValue={props.userName}
       />
       {/* Display the value of the userName State variable */}
-      <h3> Name : {userName}</h3>
+      <h3> Name : {props.userName}</h3>
     </div>
   );
 }
