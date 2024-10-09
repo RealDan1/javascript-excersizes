a651bf9ca6c62ab8b225ece28a26eb61f8d271c5
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-09 11:40:35 +0200

    feat: add submission zip

---
+++ task-5/ReactComponents/Submission/ReactComponents/react-interest-calculator/src/components/AddInterest.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+import { useState } from 'react';
+
+export default function AddInterest({ balance, setBalance }) {
+  const [interestPercent, setInterestPercent] = useState(0);
+
+  //function to handle the interest input change event
+  function handleInterestPercentChange(e) {
+    setInterestPercent(e.target.value);
+  }
+
+  function handleInterest() {
+    // add the interest amount rounded to two decimals
+    setBalance(
+      Math.round((balance + balance * (Number(interestPercent) / 100)) * 100) /
+        100
+    );
+  }
+
+  function handleFixedInterest() {
+    // add the interest amount rounded to two decimals
+
+    setBalance(Math.round((balance + balance * (10 / 100)) * 100) / 100);
+  }
+
+  return (
+    <div id="addInterest">
+      <div className="inputGroup">
+        {' '}
+        <label htmlFor="inputInterest" className="form-label">
+          Add Interest Percentage
+        </label>
+        <br />
+        <input
+          id="inputInterest"
+          type="number"
+          value={interestPercent}
+          onChange={handleInterestPercentChange}
+          placeholder="Add a percentage amount as a number"
+        />
+      </div>
+
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={handleInterest}
+        style={{ margin: '0px 10px', marginBottom: '3px' }}
+      >
+        Add Custom Interest
+      </button>
+      <p>or</p>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={handleFixedInterest}
+        style={{ margin: '0px 10px 3px 10px' }}
+      >
+        Add Fixed Interest(10%)
+      </button>
+    </div>
+  );
+}
---
+++ task-5/ReactComponents/Submission/ReactComponents/react-interest-calculator/src/components/Withdraw.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
+// Component to withdraw an amount - pull state from parent: balance, and use the balance and setBalance for calculation
+export default function Withdraw({ balance, setBalance }) {
+  // declare state var for withdraw amount
+  const [withdrawAmount, setWithdrawAmount] = useState(0);
+
+  // Function to handle the withdraw event change(on input change)
+  function handleWithdrawChange(e) {
+    setWithdrawAmount(e.target.value);
+  }
+
+  // declare function to decrease the balance by the amount
+  function handleWithdraw() {
+    setBalance(Math.round((balance - Number(withdrawAmount)) * 100) / 100);
+  }
+  return (
+    <div>
+      <div className="inputGroup">
+        {' '}
+        <label htmlFor="withdrawInput" className="form-label">
+          Withdraw Amount:{' '}
+        </label>
+        <br />
+        <input
+          id="withdrawInput"
+          type="number"
+          value={withdrawAmount}
+          onChange={handleWithdrawChange}
+          placeholder="Enter an amount for withdrawal"
+        />
+      </div>
+
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={handleWithdraw}
+        style={{ margin: '0px 10px', marginBottom: '3px' }}
+      >
+        Withdraw
+      </button>
+    </div>
+  );
+}
