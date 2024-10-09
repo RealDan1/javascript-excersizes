3d5867ecc65affe3980737a5727306f9bd337045
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-09 09:21:16 +0200

    feat: lift state of balance and drop state to separate withdraw component

--- task-5/ReactComponents/react-interest-calculator/src/components/Balance.jsx
+++ task-5/ReactComponents/react-interest-calculator/src/components/Balance.jsx
@@ -1,16 +1,9 @@
 import React, { useState } from 'react';
+import Deposit from './Deposit';
 
 export default function Balance() {
   // declare state var for balance
   let [balance, setBalance] = useState(0);
-
-  // declare state var for deposit amount
-  const [depositAmount, setDepositAmount] = useState(0);
-
-  // Function to handle the deposit event change
-  function handleDepositChange(e) {
-    setDepositAmount(e.target.value);
-  }
 
   // declare state var for withdraw amount
   const [withdrawAmount, setWithdrawAmount] = useState(0);
@@ -18,12 +11,6 @@ export default function Balance() {
   // Function to handle the withdraw event change
   function handleWithdrawChange(e) {
     setWithdrawAmount(e.target.value);
-  }
-
-  // declare function to increase the balance by the amount
-  function handleDeposit() {
-    //convert input string to a number
-    setBalance(balance + Number(depositAmount));
   }
 
   // declare function to decrease the balance by the amount
@@ -35,17 +22,6 @@ export default function Balance() {
     <div>
       <h1>Balance: {balance}</h1>
 
-      <label htmlFor="depositInput">Deposit Amount: </label>
-      <input
-        id="depositInput"
-        type="number"
-        value={depositAmount}
-        onChange={handleDepositChange}
-        placeholder="Enter a number"
-      />
-
-      <button onClick={handleDeposit}>Deposit</button>
-
       <label htmlFor="withdrawInput">Withdraw Amount: </label>
       <input
         id="withdrawInput"
@@ -56,6 +32,8 @@ export default function Balance() {
       />
 
       <button onClick={handleWithdraw}>Withdraw</button>
+
+      <Deposit balance={balance} setBalance={setBalance} />
     </div>
   );
 }
