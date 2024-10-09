f92705a94245bf3ad2a646155c88f8f986d992d0
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-09 09:55:26 +0200

    feat: finish addInterest Component

--- task-5/ReactComponents/react-interest-calculator/src/components/AddInterest.jsx
+++ task-5/ReactComponents/react-interest-calculator/src/components/AddInterest.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useState } from 'react';
 
-export default function AddInterest() {
+export default function AddInterest({ balance, setBalance }) {
   const [interestPercent, setInterestPercent] = useState(10);
 
   //function to handle the interest input change event
   function handleInterestPercentChange(e) {
     setInterestPercent(e.target.value);
+  }
+
+  function handleInterest() {
+    setBalance(balance + balance * (Number(interestPercent) / 100));
   }
 
   return (
@@ -20,7 +24,7 @@ export default function AddInterest() {
         value={interestPercent}
         onChange={handleInterestPercentChange}
       />
-      <button>Add Interest</button>
+      <button onClick={handleInterest}>Add Interest</button>
       <h1>{interestPercent}</h1>
     </div>
   );
