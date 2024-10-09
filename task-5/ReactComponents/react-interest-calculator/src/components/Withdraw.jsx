62758e251bcb807d3dfe3ef259e128cc053cd27a
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-09 09:30:18 +0200

    feat: lift state to parent: balance, and split child withdraw into separate component

---
+++ task-5/ReactComponents/react-interest-calculator/src/components/Withdraw.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useState } from 'react';
+
+// Component to withdraw an amount - pull state from parent: balance, and use the balance and setBalance for calculation
+export default function Withdraw({ balance, setBalance }) {
+  // declare state var for withdraw amount
+  const [withdrawAmount, setWithdrawAmount] = useState(0);
+
+  // Function to handle the withdraw event change(on input change)
+  function handleWithdrawChange(e) {
+    setWithdrawAmount(e.target.value);
+  }
+
+  // declare function to decrease the balance by the amount
+  function handleWithdraw() {
+    setBalance(balance - Number(withdrawAmount));
+  }
+  return (
+    <div>
+      <label htmlFor="withdrawInput">Withdraw Amount: </label>
+      <input
+        id="withdrawInput"
+        type="number"
+        value={withdrawAmount}
+        onChange={handleWithdrawChange}
+        placeholder="Enter a number"
+      />
+
+      <button onClick={handleWithdraw}>Withdraw</button>
+    </div>
+  );
+}
