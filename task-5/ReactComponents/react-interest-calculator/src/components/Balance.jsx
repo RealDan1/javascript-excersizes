56da7b5f8aad2b998a80778f27bc9e6368ab509d
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-09 09:32:36 +0200

    feat: rearrange components

--- task-5/ReactComponents/react-interest-calculator/src/components/Balance.jsx
+++ task-5/ReactComponents/react-interest-calculator/src/components/Balance.jsx
@@ -10,8 +10,8 @@ export default function Balance() {
     <div>
       <h1>Balance: {balance}</h1>
       {/* the balance state is lifted to this parent component(balance) and shared between the withdraw and deposit components */}
-      <Withdraw balance={balance} setBalance={setBalance} />
       <Deposit balance={balance} setBalance={setBalance} />
+      <Withdraw balance={balance} setBalance={setBalance} />
     </div>
   );
 }
