6ed0c57391e95eb5e7bf82bb6b020cbec53ee3be
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-09 11:40:35 +0200

    feat: add submission zip

---
+++ task-5/ReactComponents/Submission/ReactComponents/react-interest-calculator/src/components/Deposit.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Component to deposit an amount - pull state from parent: balance, and use the balance and setBalance for calculation - object destructuring is used to pass
+export default function Deposit({ balance, setBalance }) {
+  // declare state var for deposit amount
+  const [depositAmount, setDepositAmount] = useState(0);
+
+  // Function to handle the deposit event change(on input change)
+  function handleDepositChange(e) {
+    //set the preferred deposit amount
+    setDepositAmount(e.target.value);
+  }
+
+  // declare function to increase the balance by the amount
+  function handleDeposit() {
+    //convert input string to a number and add it to the balance - round the new number to two decimals
+    setBalance(Math.round((balance + Number(depositAmount)) * 100) / 100);
+  }
+
+  return (
+    <div id="deposit">
+      <div className="inputGroup">
+        <label htmlFor="depositInput" className="form-label">
+          Deposit Amount:{' '}
+        </label>
+        <br />
+        <input
+          id="depositInput"
+          type="number"
+          value={depositAmount}
+          onChange={handleDepositChange}
+          placeholder="Enter an amount for deposit"
+        />
+      </div>
+
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={handleDeposit}
+        style={{ margin: '0px 10px 3px 10px' }}
+      >
+        Deposit
+      </button>
+    </div>
+  );
+}
