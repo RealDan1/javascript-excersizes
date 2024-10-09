4a0ae512fc734757b380fc155e4546a5c99fb953
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-09 09:30:18 +0200

    feat: lift state to parent: balance, and split child withdraw into separate component

--- task-5/ReactComponents/react-interest-calculator/src/components/Balance.jsx
+++ task-5/ReactComponents/react-interest-calculator/src/components/Balance.jsx
@@ -1,38 +1,15 @@
 import React, { useState } from 'react';
 import Deposit from './Deposit';
+import Withdraw from './Withdraw';
 
 export default function Balance() {
   // declare state var for balance
   let [balance, setBalance] = useState(0);
 
-  // declare state var for withdraw amount
-  const [withdrawAmount, setWithdrawAmount] = useState(0);
-
-  // Function to handle the withdraw event change
-  function handleWithdrawChange(e) {
-    setWithdrawAmount(e.target.value);
-  }
-
-  // declare function to decrease the balance by the amount
-  function handleWithdraw() {
-    setBalance(balance - Number(withdrawAmount));
-  }
-
   return (
     <div>
       <h1>Balance: {balance}</h1>
-
-      <label htmlFor="withdrawInput">Withdraw Amount: </label>
-      <input
-        id="withdrawInput"
-        type="number"
-        value={withdrawAmount}
-        onChange={handleWithdrawChange}
-        placeholder="Enter a number"
-      />
-
-      <button onClick={handleWithdraw}>Withdraw</button>
-
+      <Withdraw balance={balance} setBalance={setBalance} />
       <Deposit balance={balance} setBalance={setBalance} />
     </div>
   );
--- task-5/ReactComponents/react-interest-calculator/src/components/Deposit.jsx
+++ task-5/ReactComponents/react-interest-calculator/src/components/Deposit.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 
+// Component to deposit an amount - pull state from parent: balance, and use the balance and setBalance for calculation
 export default function Deposit({ balance, setBalance }) {
   // declare state var for deposit amount
   const [depositAmount, setDepositAmount] = useState(0);
 
-  // Function to handle the deposit event change
+  // Function to handle the deposit event change(on input change)
   function handleDepositChange(e) {
+    //set the preferred deposit amount
     setDepositAmount(e.target.value);
   }
 
   // declare function to increase the balance by the amount
   function handleDeposit() {
-    //convert input string to a number
+    //convert input string to a number and add it to the balance
     setBalance(balance + Number(depositAmount));
   }
 
