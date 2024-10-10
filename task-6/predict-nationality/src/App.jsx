85a479eaa844d95ea0be4edc2497c7719c6ed0fd
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-10 11:28:07 +0200

    feat: complete input error checks

--- task-6/predict-nationality/src/App.jsx
+++ task-6/predict-nationality/src/App.jsx
@@ -20,11 +20,16 @@ export default function App() {
 
   //function to do the api call and store the found nationality in state
   async function handlePredict() {
-    //do the api fetch with name
-    let response = await fetch(`https://api.nationalize.io?name=${name}`);
-    let data = await response.json();
-    //set nationality as the string of the first object in the country array (data can be displayed in a more user friendly manner later - this was as the task requested)
-    setNationality(JSON.stringify(data.country[1]));
+    // if a name has been typed in:
+    if (name !== '') {
+      //do the api fetch with name
+      let response = await fetch(
+        `https://api.nationalize.io?name=${name.toLowerCase}`
+      );
+      let data = await response.json();
+      //set nationality as the string of the first object in the country array (data can be displayed in a more user friendly manner later - this was as the task requested)
+      setNationality(JSON.stringify(data.country[1]));
+    }
   }
 
   return (
@@ -45,15 +50,10 @@ export default function App() {
       {/* display nationality only if it is not empty */}
       {nationality !== '' && (
         <>
-          <h1>Your predicted nationality is:</h1>
+          <h1>The predicted nationality for {name} is:</h1>
           <h2>{nationality}</h2>
         </>
       )}
     </div>
   );
 }
-
-// Create a React app that will predict the nationality of a person given their name.
-// ● You will need an auto-focused input field, a button that will trigger a function that will fetch data from the nationalize.io API: (https://api.nationalize.io?name=<Enter name here>).
-// Here is an example of fetching the results for “Micheal”: https://api.nationalize.io?name=michael. After the fetch, please display the details of the first object in the country array.
-// ● Please ensure to only use function components and use the useState, useEffect, and useRef hooks.
