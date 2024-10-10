702cc09ebe3a2939d4cc5810689a7d7d22421715
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-10 10:15:36 +0200

    feat: add autofocus input

---
+++ react-state-intro/src/components/AutoFocus.jsx
@@ -0,0 +1,19 @@
+import { useState, useRef, useEffect } from 'react';
+
+export default function AutoFocus() {
+  const [username, setUsername] = useState('');
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
+    <div>
+      <input
+        ref={inputRef}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+    </div>
+  );
+}
