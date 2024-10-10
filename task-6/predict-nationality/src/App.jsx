57749770bea2daa94bdb41e8cb2d1595d272f377
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-10 10:30:00 +0200

    feat: change app file extension

---
+++ task-6/predict-nationality/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
+
+// Create a React app that will predict the nationality of a person given their name.
+// ● You will need an auto-focused input field, a button that will trigger a function that will fetch data from the nationalize.io API: (https://api.nationalize.io?name=<Enter name here>).
+// Here is an example of fetching the results for “Micheal”: https://api.nationalize.io?name=michael. After the fetch, please display the details of the first object in the country array.
+// ● Please ensure to only use function components and use the useState, useEffect, and useRef hooks.
