bb6a3e874afd566ac2c1538f116f8abca482736b
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-10 10:50:41 +0200

    feat: add boilerplate useref and usestate

--- task-6/predict-nationality/src/App.jsx
+++ task-6/predict-nationality/src/App.jsx
@@ -1,10 +1,43 @@
 import './App.css';
+import React from 'react';
+import { useState, useRef, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  //create name state
+  const [name, setName] = useState(null);
+
+  //create useRef for focus function
+  const inputRef = useRef();
+
+  //function to focus input
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  //create nationality state to store after api call
+  const [nationality, setNationality] = useState(null);
+
+  function handlePredict() {}
+
+  return (
+    <div className="App">
+      <label htmlFor="nameInput">Enter name here:</label>
+      {/* <br /> */}
+      <input
+        id="nameInput"
+        ref={inputRef}
+        type="text"
+        onChange={() => setName(e.target.value)}
+        value={name}
+      />
+      <br />
+      <button className="btn btn-primary" onClick={handlePredict}>
+        Predict Nationality
+      </button>
+    </div>
+  );
 }
-
-export default App;
 
 // Create a React app that will predict the nationality of a person given their name.
 // ● You will need an auto-focused input field, a button that will trigger a function that will fetch data from the nationalize.io API: (https://api.nationalize.io?name=<Enter name here>).
