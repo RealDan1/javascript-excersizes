7967f26a1990d1e1bd3a1808114bedf7f7ae8a9a
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-11 12:48:06 +0200

    feat: add skeleton for weather fetch

--- task-6-2/weather-app/src/App.jsx
+++ task-6-2/weather-app/src/App.jsx
@@ -3,12 +3,21 @@ import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
   const [city, setCity] = useState('');
+  const [weather, setWeather] = useState('');
 
   //focus the input with useRef and useEffect
   const inputRef = useRef();
   useEffect(() => {
     inputRef.current.focus();
   }, []);
+
+  const findWeather = async () => {
+    let response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${city}`
+    );
+    let data = await resonse.json();
+    setWeather(data);
+  };
 
   return (
     <div className="App">
@@ -18,11 +27,11 @@ function App() {
         onChange={(e) => setCity(e.target.value)}
         value={city}
       />
-      <button>Find Weather</button>
+      <button onClick={findWeather}>Find Weather</button>
+      <h1>The weather is:</h1>
+      <h2>{JSON.stringify(weather)}</h2>
     </div>
   );
 }
 
 export default App;
-
-// http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.WEATHER_API_KEY}`
