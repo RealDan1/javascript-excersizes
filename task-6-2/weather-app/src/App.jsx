aa9c165e91124439f0f3987ce54ed77d4a0fc203
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-11 12:37:05 +0200

    feat: add env gitignore

--- task-6-2/weather-app/src/App.jsx
+++ task-6-2/weather-app/src/App.jsx
@@ -1,7 +1,28 @@
 import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const [city, setCity] = useState('');
+
+  //focus the input with useRef and useEffect
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        ref={inputRef}
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+      />
+      <button>Find Weather</button>
+    </div>
+  );
 }
 
 export default App;
+
+// http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.WEATHER_API_KEY}`
