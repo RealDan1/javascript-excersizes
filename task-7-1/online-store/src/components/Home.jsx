8c842fc1d71cada52e03f29e571df6cfc0aa1068
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-15 16:06:03 +0200

    feat: flesh out skeleton for login and styles

--- task-7-1/online-store/src/components/Home.jsx
+++ task-7-1/online-store/src/components/Home.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function Home() {
-  return <p>This is homepage</p>;
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    let userLogin = user;
+  };
+
+  return (
+    <div className="homepage">
+      <label htmlFor="login-input">User: </label>
+      <input
+        id="login-input"
+        type="text"
+        value={user}
+        ref={inputRef}
+        onChange={(e) => {
+          setUser(e.target.value);
+        }}
+      />
+      <label htmlFor="password-input">Password:</label>
+      <input
+        id="password-input"
+        type="text"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
 }
