c5764949d7ad25fced74507d9ac5725ed05458d6
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-16 11:10:50 +0200

    feat: add duplicate for useNavigate

---
+++ task-7-1-dup/online-store/src/components/Home.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+
+export default function Home() {
+  //auto focus the user input
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  //declare user and loggedIn state variables
+  const [user, setUser] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div className="home">
+      {isLoggedIn && user !== '' ? (
+        <div className="welcome-paragraph">
+          <p>Welcome {user} </p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div className="login-container">
+          <label htmlFor="login-input">User: </label>
+          <input
+            id="login-input"
+            type="text"
+            value={user}
+            ref={inputRef}
+            onChange={(e) => {
+              setUser(e.target.value);
+            }}
+          />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      )}
+    </div>
+  );
+}
