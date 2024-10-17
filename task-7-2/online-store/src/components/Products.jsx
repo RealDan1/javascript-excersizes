7dd7889d5fe41432738ec1e591e5566df2025343
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-17 14:10:22 +0200

    feat: fix array bracket

--- task-7-2/online-store/src/components/Products.jsx
+++ task-7-2/online-store/src/components/Products.jsx
@@ -5,59 +5,58 @@ import Card from 'react-bootstrap/Card';
 
 function Products() {
   let products = [
-    
-      {
-        title: 'Milk',
-        description: '500ml Full cream Milk',
-        price: 49.99,
-      },
-      {
-        title: 'Apples',
-        description: 'Red Delicious Apples',
-        price: 29.99,
-      },
-      {
-        title: 'Bread',
-        description: 'Whole Wheat Bread',
-        price: 39.99,
-      },
-      {
-        title: 'Eggs',
-        description: 'Large Eggs',
-        price: 44.99,
-      },
-      {
-        title: 'Coffee',
-        description: 'Ground Coffee',
-        price: 119.99,
-      },
-      {
-        title: 'Pasta',
-        description: 'Spaghetti Pasta',
-        price: 24.99,
-      },
-      {
-        title: 'Tomato Ketchup',
-        description: 'Tomato Ketchup',
-        price: 19.99,
-      },
-      {
-        title: 'Olive Oil',
-        description: 'Extra Virgin Olive Oil',
-        price: 169.99,
-      },
-      {
-        title: 'Chicken Breast',
-        description: 'Boneless, Skinless Chicken Breast',
-        price: 119.99,
-      },
-      {
-        title: 'Bananas',
-        description: 'Ripe Bananas',
-        price: 24.99,
-      },
-    ];
+    {
+      title: 'Milk',
+      description: '500ml Full cream Milk',
+      price: 49.99,
+    },
+    {
+      title: 'Apples',
+      description: 'Red Delicious Apples',
+      price: 29.99,
+    },
+    {
+      title: 'Bread',
+      description: 'Whole Wheat Bread',
+      price: 39.99,
+    },
+    {
+      title: 'Eggs',
+      description: 'Large Eggs',
+      price: 44.99,
+    },
+    {
+      title: 'Coffee',
+      description: 'Ground Coffee',
+      price: 119.99,
+    },
+    {
+      title: 'Pasta',
+      description: 'Spaghetti Pasta',
+      price: 24.99,
+    },
+    {
+      title: 'Tomato Ketchup',
+      description: 'Tomato Ketchup',
+      price: 19.99,
+    },
+    {
+      title: 'Olive Oil',
+      description: 'Extra Virgin Olive Oil',
+      price: 169.99,
+    },
+    {
+      title: 'Chicken Breast',
+      description: 'Boneless, Skinless Chicken Breast',
+      price: 119.99,
+    },
+    {
+      title: 'Bananas',
+      description: 'Ripe Bananas',
+      price: 24.99,
+    },
   ];
+
   return (
     <div className="products">
       <ul>
@@ -68,11 +67,12 @@ function Products() {
                 <Card.Img variant="top" src="holder.js/100px180" />
                 <Card.Body>
                   <Card.Title>{item.title}</Card.Title>
-                  <Card.Text>Price: 
+                  <Card.Text>
+                    Price:
                     {item.price}
-                    Description: 
+                    Description:
                     {item.description}
-                    </Card.Text>
+                  </Card.Text>
                   <Button variant="primary">Buy</Button>
                 </Card.Body>
               </Card>
