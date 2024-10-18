de13254ef68f76aec05c630957378c974acf1224
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-18 11:23:51 +0200

    feat: add about totalprice

--- task-7-2/online-store/src/components/About.jsx
+++ task-7-2/online-store/src/components/About.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import TotalPrice from './TotalPrice';
 
-export default function About() {
-  return <p>This is About</p>;
+export default function About({ totalPrice }) {
+  return (
+    <div className="about">
+      <p>This is About</p>
+      {totalPrice !== 0 && <TotalPrice totalPrice={totalPrice} />}
+    </div>
+  );
 }
