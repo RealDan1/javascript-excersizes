3bf60939a5ddbd07798e4ae8084cb6f445976cfb
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-19 14:02:18 +0200

    feat: cleanup

--- task-7-2/online-store/src/components/TotalPrice.jsx
+++ task-7-2/online-store/src/components/TotalPrice.jsx
@@ -1,11 +1,4 @@
-// import React, { useState } from 'react';
-// import { useLocation } from 'react-router-dom';
-
 export default function TotalPrice({ totalPrice }) {
-  // const location = useLocation();
-
-  // const data = location.state;
-
   return (
     //only display totalPrice if its greater than 0
     totalPrice !== 0 && (
