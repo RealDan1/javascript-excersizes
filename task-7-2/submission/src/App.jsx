afe59ed9038177142b7545dee60a2aaedd47e8dc
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-20 14:40:11 +0200

    feat: add submission files

---
+++ task-7-2/submission/src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css';
+import Home from './components/Home';
+import Products from './components/Products';
+import About from './components/About';
+import { Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
+
+function App() {
+  let [totalPrice, setTotalPrice] = useState(0);
+
+  //parent app.jsx will hand down a handleTotal function to children and then in turn will display the total price data consistently across tabs (using useState)
+  function handleTotal(price) {
+    setTotalPrice(Math.round((totalPrice += price) * 100) / 100);
+  }
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/products"
+          element={
+            //hand down function and total to children
+            <Products handleTotal={handleTotal} totalPrice={totalPrice} />
+          }
+        />
+        <Route path="/about" element={<About totalPrice={totalPrice} />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
