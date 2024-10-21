419fea4011345ac390ad6f4be7795dcf5fdfd7d3
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-21 10:19:55 +0200

    feat: add formik template

--- task-8/online-store/src/components/Login.jsx
+++ task-8/online-store/src/components/Login.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import { useFormik } from 'formik';
 
 export default function Login() {
-  return <h1>This is login</h1>;
+  // Pass the initial form value and a submit function to the
+  // useFormik() hook, which returns an object of form state and
+  // helper methods
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    onSubmit: (values) => {
+      // Submit handling code goes here
+    },
+  });
+  // Rendering of the form
+  return (
+    // Note the use of the formik object below
+    <form onSubmit={formik.handleSubmit}>
+      {' '}
+      {/* // Submit handler */}
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      />
+      {/* // Tweaked error message display logic */}
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
