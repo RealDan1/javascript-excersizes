b0f23ef4357e6c7a0ae0eea9296b8503eba2524f
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-21 10:31:47 +0200

    feat: add formik password validation (generic)

--- task-8/online-store/src/components/Login.jsx
+++ task-8/online-store/src/components/Login.jsx
@@ -1,18 +1,37 @@
 import { useFormik } from 'formik';
 
 export default function Login() {
-  // Pass the initial form value and a submit function to the
-  // useFormik() hook, which returns an object of form state and
-  // helper methods
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.password) {
+      errors.password = 'Required';
+    } else if (values.password.length > 20) {
+      errors.password = 'Must be 20 characters or less';
+    }
+
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = 'Invalid email address';
+    }
+
+    return errors;
+  };
+
   const formik = useFormik({
     initialValues: {
       email: '',
+      password: '',
     },
+    validate,
     onSubmit: (values) => {
       // Submit handling code goes here
     },
   });
-  // Rendering of the form
+
   return (
     // Note the use of the formik object below
     <form onSubmit={formik.handleSubmit}>
@@ -31,7 +50,21 @@ export default function Login() {
       {formik.touched.email && formik.errors.email ? (
         <div>{formik.errors.email}</div>
       ) : null}
-      <button type="submit">Submit</button>
+      {/* // Submit handler */}
+      <label htmlFor="password">Password:</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.password}
+      />
+      {/* // Tweaked error message display logic */}
+      {formik.touched.password && formik.errors.password ? (
+        <div>{formik.errors.password}</div>
+      ) : null}
+      <button type="submit">Login</button>
     </form>
   );
 }
