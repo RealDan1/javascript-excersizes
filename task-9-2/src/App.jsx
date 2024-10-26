337f451eaa61abe343b758fed5241d0359fb50fc
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-26 14:04:59 +0200

    feat: add bootstrap and dynamic list template

--- task-9-2/src/App.jsx
+++ task-9-2/src/App.jsx
@@ -1,7 +1,26 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+let toDoArray = ['write something', 'drink something'];
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="heading">
+        <h1>To Do's:</h1>
+      </div>
+      <div className="toDoList">
+        <ul>
+          {toDoArray.map((item, key) => (
+            <li key={key}>
+              <input type="checkbox" id={key} name={key} />
+              <label htmlFor="vehicle1">{item}</label>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
