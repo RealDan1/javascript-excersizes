bb63e76b96f42d68b293328ba809e428a0c4b4ef
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-27 10:05:44 +0200

    feat: cleanup naming convention in redux

--- task-9-2/src/store/toDoState.jsx
+++ task-9-2/src/store/toDoState.jsx
@@ -1,26 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const toDoSlice = createSlice({
-  name: 'toDoItems',
+const toDoListSlice = createSlice({
+  name: 'toDoList',
   initialState: [{id:0, text: 'homework', completed: false}, {id:1,text: 'gardening'}],
   },
   reducers: {
-    add: (state, action) => {
+    addToDo: (state, action) => {
       const {id, text} = action.payload;
-      state.push({id: toDoItems.length}), // push the string to the new array item
-      state.toDoItems.push(false),//push the checkbox item as unchecked when its created
+      state.push({id: id, text: text, completed: false}), // push the string to the new array item
+      state.push(false),//push the checkbox item as unchecked when its created
     },
-    edit: (state, action) => {
-      //something here that i dont understand yet
-      state.toDoItems.splice();
+    editToDo: (state, action) => {
+      
+      state.splice();
     },
-    check: (state, action) => {
+    checkToDo: (state, action) => {
 
-state.toDoCheck.splice()//maybe splice in "true" at the index of action.payload somehow? see i havent even seen how i would get this input at the correct index yet from a standard JS/html input box let alone in react with redux
-
+state.splice()
     }
   },
 });
 
-export const { add, edit } = toDoSlice.actions;
-export default toDoSlice.reducer;
+export const { addToDo, editToDo, checkToDo } = toDoListSlice.actions;
+export default toDoListSlice.reducer;
