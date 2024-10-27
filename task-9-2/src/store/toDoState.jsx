101b9126db8a94c09cd8172caadd6cfccd16b172
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-27 12:38:29 +0200

    feaT: correct syntax highlighting issue

--- task-9-2/src/store/toDoState.jsx
+++ task-9-2/src/store/toDoState.jsx
@@ -2,32 +2,32 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const toDoListSlice = createSlice({
   name: 'toDoList',
-  initialState: [{id:0, text: 'homework', completed: false}, {id:1,text: 'gardening'}],
-  },
-  reducers: {addToDo: (state, action) => {
-      const {id, text} = action.payload;// HERE
-      state.push({id: id, text: text, completed: false}), // push the string to the new array item
-      state.push(false)//push the checkbox item as unchecked when its created
+  initialState: [
+    { id: 0, text: 'homework', completed: false },
+    { id: 1, text: 'gardening', completed: false },
+  ],
+  reducers: {
+    // HERE
+    addToDo: (state, action) => {
+      const { id, text } = action.payload; // HERE
+      state.push({ id: id, text: text, completed: false }); // push the string to the new array item
+      state.push(false); // push the checkbox item as unchecked when its created
     },
     editToDo: (state, action) => {
-      const {id, text} = action.payload;
-      state.map((item)=> {
-        // for each item, if the item id matches the id of the payload item (the item being selected to edit):
-        if (item.id == id) {
-          return {...item, text} // return the item unmodified (using spread syntax) with an updated text field
-
+      const { id, text } = action.payload;
+      state.map((item) => {
+        if (item.id === id) {
+          return { ...item, text };
         } else {
-          return item // otherwise just return the item unmodified and continue flipping through the whole toDoList until the item is found.
+          return item;
         }
-      })
-     
+      });
     },
     checkToDo: (state, action) => {
-
-state.splice()
-    }
-  }
-);
+      state.splice();
+    },
+  },
+});
 
 export const { addToDo, editToDo, checkToDo } = toDoListSlice.actions;
 export default toDoListSlice.reducer;
