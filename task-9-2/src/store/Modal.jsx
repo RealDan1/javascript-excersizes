54b9a6cbd7d8ac5219124567459bb8bf7ae8c0d0
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-30 15:09:34 +0200

    feat: add bootstrap and modal state logic

--- task-9-2/src/store/Modal.jsx
+++ task-9-2/src/store/Modal.jsx
@@ -2,31 +2,33 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import { useSelector, useDispatch } from 'react-redux';
 
-function MyModal({ id }) {
-    const [show, setShow] = useState(false);
+function MyModal({ id, handleClose, show, launchEditDispatch, editModalText }) {
+    let toDoListData = useSelector((state) => state.toDoList);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    // const [show, setShow] = useState(false);
+    // const handleClose = () => setShow(false);
+    // const handleShow = () => setShow(true);
 
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
+            {/* <Button variant="primary" onClick={handleShow}>
                 Launch demo modal
-            </Button>
+            </Button> */}
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>Enter your changes to the ToDoList item:</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        {/* <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="name@example.com" autoFocus />
-                        </Form.Group>
+                        </Form.Group> */}
                         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>Example textarea</Form.Label>
+                            <Form.Label>{toDoListData.filter((item) => item.id === id).text}</Form.Label>
                             <Form.Control as="textarea" rows={3} />
                         </Form.Group>
                     </Form>
@@ -35,7 +37,7 @@ function MyModal({ id }) {
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={() => launchEditDispatch(text)}>
                         Save Changes
                     </Button>
                 </Modal.Footer>
