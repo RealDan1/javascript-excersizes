805549faa3b806c13944a76049ef14eef93b74dc
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-10-31 11:25:45 +0200

    feat: undo array state change

--- task-9-2/src/store/toDoListSlice.jsx
+++ task-9-2/src/store/toDoListSlice.jsx
@@ -2,50 +2,53 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const toDoListSlice = createSlice({
     name: 'toDoList',
-    initialState: {
-        list: [
-            { content1: 'Content1', completed: false }, // initial to-do items
-            { content2: 'Content2', completed: false },
-        ],
-    },
+    initialState: [
+        { id: 0, text: 'Homework', completed: false },
+        { id: 1, text: 'Gardening', completed: false },
+    ],
     reducers: {
         addToDo: (state, action) => {
             const { text, id } = action.payload;
-            state.list.push({ id, text, completed: false }); // Add new item to state.list array
+            state.push({ id: id, text: text, completed: false }); // push the string to the new array item
         },
         editToDo: (state, action) => {
             const { id, text } = action.payload; // deconstruct the payload
-            state.list = state.list.map((item) => {
+            return state.map((item) => {
                 if (item.id === id) {
+                    //for each item in the state array: if the item id matches the id inside the payload:
                     return { ...item, text }; // return a copy of the original item object, plus a modified text field
                 } else {
-                    return item;
+                    return item; //otherwise just return the original item and keep flipping through the state until the item is found
                 }
             });
         },
         checkToDo: (state, action) => {
             const { id } = action.payload; //deconstruct the payload
-            state.list = state.list.map((item) => {
+            return state.map((item) => {
+                // flip through the whole state array as done previously
                 if (item.id === id) {
-                    return { ...item, completed: true }; // set completed to true if item id matches
+                    //for each item if the id matches:
+                    return { ...item, completed: true }; // return the original item plus a modified "completed" field
                 } else {
-                    return item;
+                    return item; //otherwise keep flipping through the array
                 }
             });
         },
         unCheckToDo: (state, action) => {
             const { id } = action.payload; // deconstruct the payload
-            state.list = state.list.map((item) => {
+            return state.map((item) => {
+                // flip through the whole state array as done previously
                 if (item.id === id) {
-                    return { ...item, completed: false }; // set completed to false if item id matches
+                    //for each item if the id matches:
+                    return { ...item, completed: false }; // return the original item plus a modified "completed" field
                 } else {
-                    return item;
+                    return item; //otherwise keep flipping through the array
                 }
             });
         },
         deleteToDo: (state, action) => {
             const { id } = action.payload;
-            state.list = state.list.filter((item) => item.id !== id); // filter out item by id
+            return state.filter((item) => item.id !== id); // filter - only keep the items that DON'T match the id of the send ID - return that to state
         },
     },
 });
