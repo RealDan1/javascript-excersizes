dbcc39a83da3e8c9edd4140d916a149e4b403a7c
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-11-01 15:10:26 +0200

    feat: add submission files

---
+++ task-10/testing-demo/submission/task-10/testing-demo/src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+test('App renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
