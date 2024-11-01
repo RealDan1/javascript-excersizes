52f8bd987b12f32726e7baac3f7d226c21a9e3b3
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-11-01 14:13:33 +0200

    feat: remove wrong app

--- task-10/src/Child1-snapshot.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer'; // Install react-test-renderer
-import Child1 from './components/Child1'; // Import your component
-
-test('Child1 component snapshot', () => {
-  const component = renderer.create(<Child1 />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
