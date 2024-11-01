27466f3f0789ed9dd265f165122861d6e893bc5e
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-11-01 14:25:38 +0200

    feat: add app test

--- task-10/testing-demo/src/App.test.js
+++ task-10/testing-demo/src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+import renderer from 'react-test-renderer';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('App renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
 });
