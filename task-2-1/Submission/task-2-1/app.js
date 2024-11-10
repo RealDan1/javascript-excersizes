4cfd319a5871e578e1e62bd62b7e497c87ecb492
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-11-10 17:11:31 +0200

    feat: add submission files and finish task

---
+++ task-2-1/Submission/task-2-1/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+const person = JSON.parse(fs.readFileSync('person.json', 'utf8'));
+
+app.get('/', function (req, res) {
+    res.send(`Welcome ${person.name}!`);
+});
+
+app.get('*', function (req, res, next) {
+    let err = new Error('Sorry! Can’t find that resource. Please check your URL.');
+    err.statusCode = 404;
+    next(err);
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+app.use(express.static('public'));
