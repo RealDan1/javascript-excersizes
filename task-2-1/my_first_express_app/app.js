fe2c3a59f4e7fbb31700fa03f674831abee75d35
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-11-20 14:39:34 +0200

    feat: comments

--- task-2-1/my_first_express_app/app.js
+++ task-2-1/my_first_express_app/app.js
@@ -20,6 +20,7 @@ app.get('*', function (req, res, next) {
     next(err);
 });
 
+//start server on port 3000
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
