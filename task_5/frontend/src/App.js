88c613ba64fdd36ea0149fbea513d19b744fd117
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-12-05 15:19:19 +0200

    feat: update fetchData to fetch from two different endpoints

--- task_5/frontend/src/App.js
+++ task_5/frontend/src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 function App() {
-    const [data, setData] = useState({}); // State to store first mesa
+    const [data, setData] = useState({}); // State to store first message
+    const [message, setMessage] = useState(''); // State to store second message
     useEffect(() => {
         fetchData(); // Fetch data each time the component loads
     }, []);
@@ -12,8 +13,11 @@ function App() {
         try {
             /* Sends a GET request to
       'http://localhost:5000//api/data' (backend server) */
-            const response = await axios.get('/api/data');
-            setData(response.data); // Update state with fetched data
+            const response1 = await axios.get('/api/data');
+            setData(response1.data); // Update state with fetched data
+
+            const response2 = await axios.get('/api/message');
+            setMessage(response2.data.message); // Update state with second fetched message
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -23,6 +27,7 @@ function App() {
             <header className="App-header">
                 {/* Display the message, or 'Loading...' if data is not yet fetched*/}
                 <h1>{data.message || 'Loading...'}</h1>
+                <h1>{message || 'Loading...'}</h1>
             </header>
         </div>
     );
