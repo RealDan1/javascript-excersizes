5f9e0bed6bd084d36b7e993237b3ef6b6b374793
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-12-05 13:30:02 +0200

    feat: get server running

--- task_5/backend/server.js
+++ task_5/backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 // Define the port number for the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 // Enable Cross-Origin Resource Sharing
 app.use(cors());
 // Define the route to retrieve the message
