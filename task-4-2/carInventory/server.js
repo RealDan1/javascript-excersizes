8bb97e242c76ce1f44714bf84f7abf8e3a9e3f73
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-12-09 10:48:19 +0200

    feat: start server boilerplate and add nodemon express

---
+++ task-4-2/carInventory/server.js
@@ -0,0 +1,7 @@
+//import dependencies
+//would have imported dotenv for secure DB password but not needed at this point (all open source)
+const uri =
+    'mongodb+srv://daneelvdm:<Mongoisgod1>@doncluster.uw8qd.mongodb.net/?retryWrites=true&w=majority&appName=DonCluster';
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
