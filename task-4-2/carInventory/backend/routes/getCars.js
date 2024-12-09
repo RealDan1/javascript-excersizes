97f761a1d74c52843ba08965896bce0da37ab5a9
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-12-09 10:48:19 +0200

    feat: start server boilerplate and add nodemon express

--- task-4-2/carInventory/backend/routes/getCars.js
+++ task-4-2/carInventory/backend/routes/getCars.js
@@ -2,3 +2,8 @@ const express = require('express');
 // Use built-in Express router
 const router = express.Router();
 const carController = require('../controllers/cars.controller.js');
+
+//create a new car
+router.post('/add', carController.create);
+
+module.exports = router;
