a21d1ad3af11de3581e37912841279b3b648db88
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-12-24 12:02:53 +0200

    feat: add token creation body

--- task-6-2/backend/index.js
+++ task-6-2/backend/index.js
@@ -17,7 +17,12 @@ app.post('/login', (req, res) => {
     //res.send(`Username: ${usr}\n Password: ${pwd}`)
 
     if (usr === 'zama' && pwd === 'secret') {
-        // To-do
+        payload = {
+            name: usr,
+            admin: false,
+        };
+        const token = jwt.sign(JSON.stringify(payload), 'jwt-secret', { algorithm: 'HS256' });
+        res.send({ token: token });
     } else {
         res.status(403).send({ err: 'Incorrect login!' });
     }
