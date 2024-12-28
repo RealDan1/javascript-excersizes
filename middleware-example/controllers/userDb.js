d271de7c29d5848e0e8be1179cdc5212a0d7ce95
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-12-28 16:03:18 +0200

    feat: add middleware

--- middleware-example/controllers/userDb.js
+++ middleware-example/controllers/userDb.js
@@ -1,7 +1,7 @@
 // controllers/userDB.js
 const userInformation = [
     { id: 1, username: 'user1', password: 'password1', todos: ['eat', 'sleep', 'code'] },
-    { id: 2, username: 'user2', password: 'password2', todos: ['By groceries', 'Food the dog', 'Make dinner'] },
+    { id: 2, username: 'user2', password: 'password2', todos: ['Buy groceries', 'Food the dog', 'Make dinner'] },
     // ... other user data
 ];
 // Export the userInformation array to be used in userController.js
