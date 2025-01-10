efccac22349fbdfc983965fb3db0603d0841b006
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-01-10 14:18:16 +0200

    feat: add skeleton/outline for itunes API search request

---
+++ task-8/itunes-search/backend/controllers/itunesApiController.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+
+const searchItunes = async (req, res) => {
+    const isFirstRequest = req.isFirstRequest;
+    const { searchTerm, mediaType } = req.params; // deconstruct the searchTerm and Mediatype from the req.params.
+
+    if (isFirstRequest) {
+        //create a new payload with id Date.now - just to have something to sign into the token
+        // Create a JWT token payload
+        //--------------------------------
+        const payload = {
+            name: userName,
+            admin: false,
+        };
+        // Generate the token for the first time -  sign the token with the payload
+        const token = jwt.sign(JSON.stringify(payload), 'HyperionDev', {
+            algorithm: 'HS256',
+            expiresIn: '3h', // Set token expiration to 3h for dev purposes
+        });
+
+        //do the request with the searchTerm and mediaType:
+        //api request goes here:
+        //-----------------------------------------
+
+        try {
+            const itunesResult = {}; //here
+            //return the result and the token in the response object
+            res.json({ itunesResult: itunesResult, token: token });
+        } catch (error) {}
+    } else {
+        //otherwise its not the first request and should include a token with an id:
+        const { id } = req.payload;
+        //do the request with the searchTerm and mediaType:
+        //api request goes here:
+        //-----------------------------------------
+        try {
+            const itunesResult = {}; //here
+            //return the result and the token in the response object
+            res.json({ itunesResult: itunesResult, token: token });
+        } catch (error) {}
+    }
+};
