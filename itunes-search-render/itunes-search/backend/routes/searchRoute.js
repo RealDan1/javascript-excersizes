03851779eef65d78eb9f2d0cb3b1b23348836de6
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-01-31 15:59:10 +0200

    feat: remove files

--- itunes-search-render/itunes-search/backend/routes/searchRoute.js
+++
@@ -1,8 +0,0 @@
-const { searchItunes } = require('../controllers/itunesApiController');
-const { jwtMiddleware } = require('../middleware/jwtMiddleware');
-
-const searchRoute = (app) => {
-    app.get('/search', jwtMiddleware, searchItunes);
-};
-
-module.exports = searchRoute;
