9f4e581ff890d0924ead18268cdd84eda68cf5b0
Daneel Van Der Merwe <69673349+RealDan1@users.noreply.github.com>
2025-02-12 12:50:15 +0000

    feat: start form of todo

--- interview-practice/to-do/to-do/src/App.jsx
+++ interview-practice/to-do/to-do/src/App.jsx
@@ -3,9 +3,23 @@ import React from 'react'
 import { useState } from 'react'
 
 function App() {
+  const [toDos, setToDos] = useState([
+  ])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    //reset the form:
+    event.target.reset();
+  }
 
   return (
     <>
+    <form onSubmit={handleSubmit}>
+      <input name='addToDo' />
+      
+
+    </form>
     </>
   )
 }
