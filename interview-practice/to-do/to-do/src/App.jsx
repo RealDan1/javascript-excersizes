d42dc8f438cec20f1856cda3d57bc6f418011a1b
Daneel Van Der Merwe <69673349+RealDan1@users.noreply.github.com>
2025-02-12 13:50:32 +0000

    feat: add map and addToDo to ToDo

--- interview-practice/to-do/to-do/src/App.jsx
+++ interview-practice/to-do/to-do/src/App.jsx
@@ -1,27 +1,58 @@
-import './App.css'
-import React from 'react'
-import { useState } from 'react'
+import "./App.css";
+import React from "react";
+import { useState } from "react";
 
 function App() {
   const [toDos, setToDos] = useState([
-  ])
+    {
+      id: crypto.randomUUID(),
+      text: "Gardening",
+      isComplete: false,
+    },
+  ]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
+    const value = event.target.toDoInput.value;
+    setToDos([
+      ...toDos,
+      {
+        id: crypto.randomUUID,
+        text: value,
+        isComplete: false,
+      },
+    ]);
     //reset the form:
     event.target.reset();
-  }
+  };
 
   return (
     <>
-    <form onSubmit={handleSubmit}>
-      <input name='addToDo' />
-      
-
-    </form>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="toDoInput">
+          <input
+            type="text"
+            name="toDoInput"
+            id="toDoInput"
+            placeholder="Enter To Do here"
+          />
+        </label>
+        <button type="submit">Add To-Do</button>
+      </form>
+      <div className="toDos">
+        {toDos.length > 0
+          ? toDos.map((item) => (
+              <li id={item.id}>
+                <p>
+                  {item.text}
+                  {item.completed ? "is completed" : "is not completed"}
+                </p>
+              </li>
+            ))
+          : "Looks real empty here, add a To Do"}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
