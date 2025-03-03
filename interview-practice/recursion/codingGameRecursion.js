ae897f0fd0e5b5abb010f776273c13d80babbf91
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-03-03 15:07:57 +0200

    feat: add check all array values

--- interview-practice/recursion/codingGameRecursion.js
+++ interview-practice/recursion/codingGameRecursion.js
@@ -26,3 +26,24 @@ function factorial(n) {
 }
 
 console.log(factorial(5));
+
+//
+var allAreLessThanSeven = all([1, 2, 9], function (num) {
+    return num < 7;
+});
+
+console.log(allAreLessThanSeven); // false
+
+//Check all values in an array
+function all(array, callback) {
+    var copy = copy || array.slice(); // shallow copies array
+
+    if (copy.length === 0) return true;
+
+    if (callback(copy[0])) {
+        copy.shift(); // remove first element from array
+        return all(copy, callback);
+    } else {
+        return false;
+    }
+}
