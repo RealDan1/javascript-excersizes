284b0f088e3fdd15bb60a5544614d68b55efeeae
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-03-05 15:47:32 +0200

    feat: scaffold starting function for iterative fibonacci

---
+++ interview-practice/fibonacci/fibonacci.js
@@ -0,0 +1,6 @@
+// iterative solution:
+//a function that takes in a number and returns an array that prints that many numbers from the fib sequence
+function fibsI(n) {
+    //iterate n times
+    for (let i = 0; i < n; i++) {}
+}
