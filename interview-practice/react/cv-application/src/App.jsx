88c56781a659b2aa73e98a4f4343536301839802
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-04-02 14:36:13 +0200

    feat: add basic components structure

--- interview-practice/react/cv-application/src/App.jsx
+++ interview-practice/react/cv-application/src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css';
-
+import Form from './components/Form';
+import Cv from './components/cv';
 function App() {
-  return <></>;
+  return (
+    <>
+      <Form></Form>
+      <Cv></Cv>
+    </>
+  );
 }
 
 export default App;
