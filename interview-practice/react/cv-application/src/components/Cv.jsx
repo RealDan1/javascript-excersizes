b88b210c69428639da9a3093776913a953ee23ec
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-04-03 14:42:50 +0200

    feat: add nested object structure for inputs

--- interview-practice/react/cv-application/src/components/Cv.jsx
+++ interview-practice/react/cv-application/src/components/Cv.jsx
@@ -1,3 +1,3 @@
-export default function Cv({ person }) {
-  return <div>{person?.name}'s CV</div>;
+export default function Cv({ cv }) {
+  return <div>{cv?.general?.name}'s CV</div>;
 }
--- interview-practice/react/cv-application/src/components/Form.jsx
+++ interview-practice/react/cv-application/src/components/Form.jsx
@@ -1,25 +1,44 @@
 import { useState } from 'react';
 import Cv from './cv';
 export default function Form() {
-  const [person, setPerson] = useState({});
-  const [input, setInput] = useState({ name: '' });
+  const [cv, setCv] = useState({});
+  const [input, setInput] = useState({ general: { name: '' }, education: { schoolName: '' } });
 
+  //assigning the values to input:
   const handleChange = (e) => {
-    const { value, name } = e.target;
-    setInput((prevInput) => ({ ...prevInput, [name]: value }));
+    const { name, value } = e.target;
+    //generalName
+    if (name === 'generalName') {
+      setInput((prevInput) => {
+        return { ...prevInput, general: { ...prevInput.general, name: value } };
+      });
+    }
+    //school name
+    else if (name === 'schoolName') {
+      setInput((prevInput) => {
+        return {
+          ...prevInput,
+          education: {
+            ...prevInput.education,
+            schoolName: value,
+          },
+        };
+      });
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setPerson({ ...input }); //maybe change this to not mutate?
+    setCv({ ...input }); //maybe change this to not mutate?
   };
 
   return (
     <>
-      <Cv person={person}></Cv>
+      <Cv cv={cv}></Cv>
       <div id="form">
         <form onSubmit={handleSubmit}>
-          <input type="text" name="name" value={input.name} onChange={handleChange} />
+          <input type="text" name="generalName" value={input.general.name} onChange={handleChange} />
+          <input type="text" name="schoolName" value={input.education.schoolName} onChange={handleChange} />
           <button type="submit">Submit</button>
         </form>
       </div>
